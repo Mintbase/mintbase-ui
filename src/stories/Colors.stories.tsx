f4d7c4dd40d756777b9e76c8026ec2fb1c33f62c
Rui Santiago <ruisantiago@Ruis-MacBook-Pro.local>
2021-11-16 14:27:31 +0000

    wip: added color stories file for storybook

---
+++ src/stories/Colors.stories.tsx
@@ -0,0 +1,186 @@
+export default {
+  title: "Core/Colors",
+};
+
+export const Colors = () => (
+  <>
+    <section className="mb-4">
+      <p className="text-xs">* all the classes must have the prefix 'bg-'</p>
+    </section>
+    <section className="mb-8">
+      <p className="text-xl font-bold mb-4">Brand</p>
+      <div className="flex gap-8">
+        <div>
+          <div className="w-24 h-24 bg-mbBlackblue rounded-md"></div>
+          <div className="text-sm font-bold">mbBlackblue</div>
+          <div className="text-sm">#070C2B</div>
+        </div>
+        <div>
+          <div className="w-24 h-24 bg-mbRed rounded-md"></div>
+          <div className="text-sm font-bold">mbRed</div>
+          <div className="text-sm">#FF2424</div>
+        </div>
+        <div>
+          <div className="w-24 h-24 bg-mbRed35 rounded-md"></div>
+          <div className="text-sm font-bold">mbRed35</div>
+          <div className="text-sm">#3A1C2A</div>
+        </div>
+        <div>
+          <div className="w-24 h-24 bg-mbRed15 rounded-md"></div>
+          <div className="text-sm font-bold">mbRed15</div>
+          <div className="text-sm">#FFDEDE</div>
+        </div>
+      </div>
+    </section>
+    <section className="mb-8">
+      <p className="text-xl font-bold mb-4">Neutral</p>
+      <div className="flex flex-wrap gap-8">
+        <div>
+          <div className="w-24 h-24 bg-gray-900 rounded-md"></div>
+          <div className="text-sm font-bold">gray-900</div>
+          <div className="text-sm">#101223</div>
+        </div>
+        <div>
+          <div className="w-24 h-24 bg-gray-850 rounded-md"></div>
+          <div className="text-sm font-bold">gray-850</div>
+          <div className="text-sm">#1E2030</div>
+        </div>
+        <div>
+          <div className="w-24 h-24 bg-gray-800 rounded-md"></div>
+          <div className="text-sm font-bold">gray-800</div>
+          <div className="text-sm">#282A3A</div>
+        </div>
+        <div>
+          <div className="w-24 h-24 bg-gray-700 rounded-md"></div>
+          <div className="text-sm font-bold">gray-700</div>
+          <div className="text-sm">#404252</div>
+        </div>
+        <div>
+          <div className="w-24 h-24 bg-gray-600 rounded-md"></div>
+          <div className="text-sm font-bold">gray-600</div>
+          <div className="text-sm">#5B5D6B</div>
+        </div>
+        <div>
+          <div className="w-24 h-24 bg-gray-500 rounded-md"></div>
+          <div className="text-sm font-bold">gray-500</div>
+          <div className="text-sm">#777986</div>
+        </div>
+        <div>
+          <div className="w-24 h-24 bg-gray-400 rounded-md"></div>
+          <div className="text-sm font-bold">gray-400</div>
+          <div className="text-sm">#9496A1</div>
+        </div>
+        <div>
+          <div className="w-24 h-24 bg-gray-300 rounded-md"></div>
+          <div className="text-sm font-bold">gray-300</div>
+          <div className="text-sm">#B3B5BD</div>
+        </div>
+        <div>
+          <div className="w-24 h-24 bg-gray-200 rounded-md"></div>
+          <div className="text-sm font-bold">gray-200</div>
+          <div className="text-sm">#D2D4DA</div>
+        </div>
+        <div>
+          <div className="w-24 h-24 bg-gray-150 rounded-md"></div>
+          <div className="text-sm font-bold">gray-150</div>
+          <div className="text-sm">#E8EAF0</div>
+        </div>
+        <div>
+          <div className="w-24 h-24 bg-gray-100 rounded-md"></div>
+          <div className="text-sm font-bold">gray-100</div>
+          <div className="text-sm">#F3F4F8</div>
+        </div>
+        <div>
+          <div className="w-24 h-24 bg-gray-50 rounded-md"></div>
+          <div className="text-sm font-bold">gray-50</div>
+          <div className="text-sm">#F9F9F9</div>
+        </div>
+      </div>
+    </section>
+    <section className="mb-8">
+      <p className="text-xl font-bold mb-4">Actions</p>
+      <div className="flex flex-wrap gap-8">
+        <div>
+          <div className="w-24 h-24 bg-blue-300-main rounded-md"></div>
+          <div className="text-sm font-bold">blue-300-main</div>
+          <div className="text-sm">#4F58A3</div>
+        </div>
+        <div>
+          <div className="w-24 h-24 bg-blue-300-35 rounded-md"></div>
+          <div className="text-sm font-bold">blue-300-35</div>
+          <div className="text-sm">#C2C5DD</div>
+        </div>
+        <div>
+          <div className="w-24 h-24 bg-blue-300-15 rounded-md"></div>
+          <div className="text-sm font-bold">blue-300-15</div>
+          <div className="text-sm">#EBECFB</div>
+        </div>
+        <div>
+          <div className="w-24 h-24 bg-blue-100-main rounded-md"></div>
+          <div className="text-sm font-bold">blue-100-main</div>
+          <div className="text-sm">#C5D0FF</div>
+        </div>
+        <div>
+          <div className="w-24 h-24 bg-blue-100-35 rounded-md"></div>
+          <div className="text-sm font-bold">blue-100-35</div>
+          <div className="text-sm">#3F4254</div>
+        </div>
+        <div>
+          <div className="w-24 h-24 bg-blue-100-15 rounded-md"></div>
+          <div className="text-sm font-bold">blue-100-15</div>
+          <div className="text-sm">#F6F8FE</div>
+        </div>
+      </div>
+    </section>
+    <section className="mb-8">
+      <p className="text-xl font-bold mb-4">State</p>
+      <div className="flex flex-wrap gap-8">
+        <div>
+          <div className="w-24 h-24 bg-success-300 rounded-md"></div>
+          <div className="text-sm font-bold">success-300</div>
+          <div className="text-sm">#0A7D6C</div>
+        </div>
+        <div>
+          <div className="w-24 h-24 bg-success-100 rounded-md"></div>
+          <div className="text-sm font-bold">success-100</div>
+          <div className="text-sm">#C8FDBD</div>
+        </div>
+        <div>
+          <div className="w-24 h-24 bg-warning-300 rounded-md"></div>
+          <div className="text-sm font-bold">warning-300</div>
+          <div className="text-sm">#FFE855</div>
+        </div>
+        <div>
+          <div className="w-24 h-24 bg-warning-100 rounded-md"></div>
+          <div className="text-sm font-bold">warning-100</div>
+          <div className="text-sm">#FFF4CD</div>
+        </div>
+        <div>
+          <div className="w-24 h-24 bg-error-300 rounded-md"></div>
+          <div className="text-sm font-bold">error-300</div>
+          <div className="text-sm">#C74C4C</div>
+        </div>
+        <div>
+          <div className="w-24 h-24 bg-error-100 rounded-md"></div>
+          <div className="text-sm font-bold">error-100</div>
+          <div className="text-sm">#ED5A5A</div>
+        </div>
+      </div>
+    </section>
+    <section className="mb-8">
+      <p className="text-xl font-bold mb-4">Shades</p>
+      <div className="flex flex-wrap gap-8">
+        <div>
+          <div className="w-24 h-24 bg-black rounded-md"></div>
+          <div className="text-sm font-bold">black</div>
+          <div className="text-sm">#000000</div>
+        </div>
+        <div>
+          <div className="w-24 h-24 bg-white rounded-md border-2 border-black"></div>
+          <div className="text-sm font-bold">white</div>
+          <div className="text-sm">#ffffff</div>
+        </div>
+      </div>
+    </section>
+  </>
+);
