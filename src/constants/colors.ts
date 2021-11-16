04f7cd1afad719424a5ccf7d9f9557651f6ced78
Rui Santiago <ruisantiagomr@gmail.com>
2021-11-16 16:04:25 +0000

    added colors constant

---
+++ src/constants/colors.ts
@@ -0,0 +1,149 @@
+import { TColorDetail } from "../types/colors.type";
+
+export const colorsArray: TColorDetail[] = [
+  {
+    sectionTitle: "Brand",
+    colors: [
+      {
+        class: "mb-blackblue",
+        hex: "#070C2B",
+      },
+      {
+        class: "mb-red",
+        hex: "#FF2424",
+      },
+      {
+        class: "mb-red-35",
+        hex: "#3A1C2A",
+      },
+      {
+        class: "mb-red-15",
+        hex: "#FFDEDE",
+      },
+    ],
+  },
+  {
+    sectionTitle: "Neutral",
+    colors: [
+      {
+        class: "gray-900",
+        hex: "#101223",
+      },
+      {
+        class: "gray-850",
+        hex: "#1E2030",
+      },
+      {
+        class: "gray-800",
+        hex: "#282A3A",
+      },
+      {
+        class: "gray-700",
+        hex: "#404252",
+      },
+      {
+        class: "gray-600",
+        hex: "#404252",
+      },
+      {
+        class: "gray-500",
+        hex: "#777986",
+      },
+      {
+        class: "gray-400",
+        hex: "#9496A1",
+      },
+      {
+        class: "gray-300",
+        hex: "#B3B5BD",
+      },
+      {
+        class: "gray-200",
+        hex: "#D2D4DA",
+      },
+      {
+        class: "gray-150",
+        hex: "#E8EAF0",
+      },
+      {
+        class: "gray-100",
+        hex: "#F3F4F8",
+      },
+      {
+        class: "gray-50",
+        hex: "#F9F9F9",
+      },
+    ],
+  },
+  {
+    sectionTitle: "Actions",
+    colors: [
+      {
+        class: "blue-300-default",
+        hex: "#4F58A3",
+      },
+      {
+        class: "blue-300-35",
+        hex: "#C2C5DD",
+      },
+      {
+        class: "blue-300-15",
+        hex: "#EBEDFB",
+      },
+      {
+        class: "blue-100-default",
+        hex: "#C5D0FF",
+      },
+      {
+        class: "blue-100-35",
+        hex: "#3F4254",
+      },
+      {
+        class: "blue-100-15",
+        hex: "#2B2E42",
+      },
+    ],
+  },
+  {
+    sectionTitle: "State",
+    colors: [
+      {
+        class: "success-300",
+        hex: "#0A7D6C",
+      },
+      {
+        class: "success-100",
+        hex: "#9FED8F",
+      },
+      {
+        class: "warning-300",
+        hex: "#FFE855",
+      },
+      {
+        class: "warning-100",
+        hex: "#FFF4CD",
+      },
+      {
+        class: "error-300",
+        hex: "#C74C4C",
+      },
+      {
+        class: "error-100",
+        hex: "#ED5A5A",
+      },
+    ],
+  },
+  {
+    sectionTitle: "Shades",
+    colors: [
+      {
+        class: "black",
+        hex: "#000000",
+      },
+      {
+        class: "white",
+        hex: "#ffffff",
+      },
+    ],
+  },
+];
