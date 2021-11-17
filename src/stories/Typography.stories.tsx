b87f3d38d5da28d59da8ce843ec73373a964991f
Rui Santiago <ruisantiagomr@gmail.com>
2021-11-17 15:38:03 +0000

    fix: change fonts path (#9)

--- src/stories/Typography.stories.tsx
+++ src/stories/Typography.stories.tsx
@@ -2,4 +2,178 @@ export default {
   title: "Core/Typography",
 };
 
-export const Typography = () => <></>;
+export const Typography = () => (
+  <>
+    <p className="text-xl font-bold font-authentic-sans-130">Typography</p>
+    <p className="text-sm mb-4 font-authentic-sans-130">
+      font-family: Authentic Sans
+    </p>
+    <table>
+      <thead>
+        <tr>
+          <th>Style</th>
+          <th>Tailwind Class</th>
+          <th>Weight</th>
+          <th>Size D/M</th>
+          <th>Line Height D/M</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Heading display 130</td>
+          <td>authentic-sans-130</td>
+          <td>Bold</td>
+          <td>35/29px</td>
+          <td>38/34px</td>
+        </tr>
+        <tr>
+          <td>Heading display 90</td>
+          <td>authentic-sans-90</td>
+          <td>Regular</td>
+          <td>35/29px</td>
+          <td>38/34px</td>
+        </tr>
+        <tr>
+          <td>Heading H1 130</td>
+          <td>authentic-sans-130</td>
+          <td>Bold</td>
+          <td>29/24px</td>
+          <td>32/28px</td>
+        </tr>
+        <tr>
+          <td>Heading H1 90</td>
+          <td>authentic-sans-90</td>
+          <td>Regular</td>
+          <td>29/24px</td>
+          <td>32/28px</td>
+        </tr>
+        <tr>
+          <td>Heading H2 130</td>
+          <td>authentic-sans-130</td>
+          <td>Bold</td>
+          <td>24/20px</td>
+          <td>24/22px</td>
+        </tr>
+        <tr>
+          <td>Heading H2 90</td>
+          <td>authentic-sans-90</td>
+          <td>Regular</td>
+          <td>24/20px</td>
+          <td>24/22px</td>
+        </tr>
+        <tr>
+          <td>Heading H3 130</td>
+          <td>authentic-sans-130</td>
+          <td>Bold</td>
+          <td>20/17px</td>
+          <td>22/18px</td>
+        </tr>
+        <tr>
+          <td>Heading H3 90</td>
+          <td>authentic-sans-90</td>
+          <td>Regular</td>
+          <td>20/17px</td>
+          <td>22/18px</td>
+        </tr>
+        <tr>
+          <td>Subheading P 130</td>
+          <td>authentic-sans-130</td>
+          <td>Bold</td>
+          <td>18/-px</td>
+          <td>22/-px</td>
+        </tr>
+        <tr>
+          <td>Subheading P 90</td>
+          <td>authentic-sans-90</td>
+          <td>Regular</td>
+          <td>18/-px</td>
+          <td>22/-px</td>
+        </tr>
+        <tr>
+          <td>Big P 130</td>
+          <td>authentic-sans-130</td>
+          <td>Bold</td>
+          <td>16/-px</td>
+          <td>18/-px</td>
+        </tr>
+        <tr>
+          <td>Big P 90</td>
+          <td>authentic-sans-90</td>
+          <td>Regular</td>
+          <td>16/-px</td>
+          <td>18/-px</td>
+        </tr>
+        <tr>
+          <td>Medium P 130</td>
+          <td>authentic-sans-130</td>
+          <td>Bold</td>
+          <td>14/-px</td>
+          <td>16/-px</td>
+        </tr>
+        <tr>
+          <td>Medium P 90</td>
+          <td>authentic-sans-90</td>
+          <td>Regular</td>
+          <td>14/-px</td>
+          <td>26/-px</td>
+        </tr>
+        <tr>
+          <td>Small P 130</td>
+          <td>authentic-sans-130</td>
+          <td>Bold</td>
+          <td>12/-px</td>
+          <td>14/-px</td>
+        </tr>
+        <tr>
+          <td>Small P 90</td>
+          <td>authentic-sans-90</td>
+          <td>Regular</td>
+          <td>12/-px</td>
+          <td>14/-px</td>
+        </tr>
+        <tr>
+          <td>Big Caption 130</td>
+          <td>authentic-sans-130</td>
+          <td>Bold</td>
+          <td>10/-px</td>
+          <td>12/-px</td>
+        </tr>
+        <tr>
+          <td>Big Caption 90</td>
+          <td>authentic-sans-90</td>
+          <td>Regular</td>
+          <td>10/-px</td>
+          <td>12/-px</td>
+        </tr>
+        <tr>
+          <td>Medium Caption 130</td>
+          <td>authentic-sans-130</td>
+          <td>Bold</td>
+          <td>9/-px</td>
+          <td>11/-px</td>
+        </tr>
+        <tr>
+          <td>Medium Caption 90</td>
+          <td>authentic-sans-90</td>
+          <td>Regular</td>
+          <td>9/-px</td>
+          <td>11/-px</td>
+        </tr>
+        <tr>
+          <td>Small Caption 130</td>
+          <td>authentic-sans-130</td>
+          <td>Bold</td>
+          <td>8/-px</td>
+          <td>10/-px</td>
+        </tr>
+        <tr>
+          <td>Small Caption 90</td>
+          <td>authentic-sans-90</td>
+          <td>Regular</td>
+          <td>8/-px</td>
+          <td>10/-px</td>
+        </tr>
+      </tbody>
+    </table>
+  </>
+);
