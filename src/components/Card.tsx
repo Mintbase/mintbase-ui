4a95277c7021f173da570e4bcf6d8ab6909d7f99
Rui Santiago <ruisantiagomr@gmail.com>
2021-11-24 11:06:13 +0000

    wip: add css properties

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -3,7 +3,7 @@ import { HeartIcon } from "@heroicons/react/outline";
 import { DotsHorizontalIcon } from "@heroicons/react/solid";
 import { DocumentDuplicateIcon } from "@heroicons/react/outline";
 
-const BASE_CARD = `bg-mb-blackblue dark:bg-white rounded p-12 mt-4 h-1/5 w-1/5 max-w-md mx-auto`;
+const BASE_CARD = `dark:bg-gray-850 bg-white rounded p-12 mt-4 max-w-md mx-auto dark:hover:bg-gray-800 hover:bg-gray-50 cursor-pointer`;
 
 type CardProps = React.ComponentProps<"div"> & {
   loading?: boolean;
@@ -38,14 +38,18 @@ const Card = ({ loading = false, title = "" }: CardProps) => {
   if (loading) return <LoadingCard />;
   return (
     <div className={`${BASE_CARD} ${loading && `animate-pulse`}`}>
-      <div className="flex flex-row justify-between text-gray-200 mb-12">
-        <HeartIcon className="h-5 w-5" />
+      <div className="flex flex-row justify-between items-center text-black dark:text-white mb-12">
+        <div className="flex space-x-12 items-center">
+          <HeartIcon className="h-4 w-4" />
+          <p className="big-cap text-gray-700 dark:text-gray-300">2</p>
+        </div>
+
         <DotsHorizontalIcon className="h-5 w-5" />
       </div>
       <div className="flex flex-col justify-center items-center">
         <div className="h-48 w-full rounded bg-mb-red"></div>
       </div>
-      <div className="flex flex-row justify-between text-gray-200 mt-12">
+      <div className="flex flex-row justify-between text-black dark:text-white mt-12">
         <div className="big-p">{title}</div>
         <div className="big-p">1 N</div>
       </div>
