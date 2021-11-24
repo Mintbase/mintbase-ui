ea8d523aab3bbeb81cb7ee9d0491c499b3be5636
Rui Santiago <ruisantiagomr@gmail.com>
2021-11-24 11:24:13 +0000

    feat: add different bg colors for dark/light storybook mode

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -4,8 +4,8 @@ export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   options: {
     storySort: {
-      order: ['Core', 'Components']
-    }
+      order: ["Core", "Components"],
+    },
   },
   themes: {
     clearable: false,
@@ -21,6 +21,27 @@ export const parameters = {
         // The class dark will be added to the body tag
         class: ["dark"],
         color: "#000000",
+      },
+    ],
+  },
+  backgrounds: {
+    default: "light-1",
+    values: [
+      {
+        name: "ligth-1",
+        value: "#ffffff",
+      },
+      {
+        name: "ligth-2",
+        value: "#F3F4F8",
+      },
+      {
+        name: "dark-1",
+        value: "#101223",
+      },
+      {
+        name: "dark-2",
+        value: "#1E2030",
       },
     ],
   },
