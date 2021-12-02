b9bf94ba49aa3b9866eb3122773ab66ce0de3603
Rui Santiago <ruisantiagomr@gmail.com>
2021-12-02 16:07:31 +0000

    feat: icons component

---
+++ src/components/icon/Icon.tsx
@@ -0,0 +1,26 @@
+import "material-icons/iconfont/material-icons.css";
+import { EIconName } from "../../constants/icons";
+
+const Icon = ({
+  name,
+  color = "mb-blackblue",
+  size = "24px",
+}: {
+  name: EIconName;
+  color: string;
+  size?: string;
+}) => {
+  return (
+    <>
+      <span
+        className={`material-icons text-${color}`}
+        style={{ fontSize: size }}
+      >
+        {name}
+      </span>
+      {/* <span className="material-icons-outlined">pie_chart</span>{" "} */}
+    </>
+  );
+};
+
+export default Icon;
