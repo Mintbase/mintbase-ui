d6fc8b4328bb5d6b48e5f080e553e8f1198adff7
Rui Santiago <ruisantiagomr@gmail.com>
2021-12-06 16:28:51 +0000

    wip: add info card component

---
+++ src/components/cards/info/InfoCard.tsx
@@ -0,0 +1,50 @@
+import { EIconName } from "../../../constants/icons";
+import Icon from "../../icon/Icon";
+import "./infocard.css";
+
+const InfoCard = ({
+  title,
+  description,
+  upperIcon = EIconName.NONE,
+  bigDescription = true,
+  handleDescriptionClick,
+}: {
+  title: string;
+  description?: string;
+  upperIcon?: EIconName;
+  bigDescription: boolean;
+  handleDescriptionClick?: () => void;
+}) => {
+  return (
+    <div className="info-card">
+      <div className="flex justify-between items-center pb-12 relative">
+        <div className="small-p text-gray-700 dark:text-gray-300">{title}</div>
+
+        {upperIcon && upperIcon !== EIconName.NONE && (
+          <div className="absolute right-0 mt-4">
+            <Icon
+              name={upperIcon}
+              size="20px"
+              color="blue-300"
+              darkColor="blue-100"
+            ></Icon>
+          </div>
+        )}
+      </div>
+      {description && (
+        <div
+          className={`${bigDescription ? "sub-p" : "med-p"} ${
+            handleDescriptionClick
+              ? "text-blue-300 dark:text-blue-100 cursor-pointer"
+              : "text-black dark:text-white"
+          }`}
+          onClick={handleDescriptionClick}
+        >
+          {description}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default InfoCard;
