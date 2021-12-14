00612aebfdd53bc7a9b659ba3c811da758b6e063
João Loureiro <loureiro.jpc@gmail.com>
2021-12-14 17:47:06 +0000

    add storybook themes

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -8,4 +8,42 @@ export const parameters = {
       date: /Date$/,
     },
   },
+  themes: {
+    clearable: false,
+    list: [
+      {
+        name: "Light",
+        class: [],
+        color: "#ffffff",
+        default: true,
+      },
+      {
+        name: "Dark",
+        // The class dark will be added to the body tag
+        class: ["dark"],
+        color: "#000000",
+      },
+    ],
+  },
+  backgrounds: {
+    default: "light-1",
+    values: [
+      {
+        name: "light-1",
+        value: "#ffffff",
+      },
+      {
+        name: "light-2",
+        value: "#F3F4F8",
+      },
+      {
+        name: "dark-1",
+        value: "#101223",
+      },
+      {
+        name: "dark-2",
+        value: "#1E2030",
+      },
+    ],
+  }
 }
