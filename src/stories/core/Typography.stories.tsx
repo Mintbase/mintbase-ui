54ed614b129ad58acc6f7ce18433c103c2587810
Rui Santiago <ruisantiagomr@gmail.com>
2021-12-15 12:23:54 +0000

    fix: new text class names accoridng to ux/ui team

--- src/stories/core/Typography.stories.tsx
+++ src/stories/core/Typography.stories.tsx
@@ -6,34 +6,34 @@ export default {
 
 export const Typography = () => (
   <section>
-    <MbText className="heading-bold mb-8">Typography</MbText>
-    <MbText className="small-p mb-12">font-family: Authentic Sans</MbText>
-    <MbText className="med-p mb-8 mt-8">
+    <MbText className="heading-130 mb-8">Typography</MbText>
+    <MbText className="p-small-90 mb-12">font-family: Authentic Sans</MbText>
+    <MbText className="p-med-90 mb-8 mt-8">
       use the class in MbText component:
       {`<MbText className="[typoname]"></MbText>`}
     </MbText>
-    <MbText className="heading-bold mb-2">heading-bold</MbText>
-    <MbText className="heading mb-2">heading</MbText>
-    <MbText className="h1-bold mb-2">h1-bold</MbText>
-    <MbText className="h1 mb-2">h1</MbText>
-    <MbText className="h2-bold mb-2">h2-bold</MbText>
-    <MbText className="h2 mb-2">h2</MbText>
-    <MbText className="h3-bold mb-2">h3-bold</MbText>
-    <MbText className="h3 mb-2">h3</MbText>
-    <MbText className="sub-p-bold mb-2">sub-p-bold</MbText>
-    <MbText className="sub-p mb-2">sub-p</MbText>
-    <MbText className="big-p-bold mb-2">big-p-bold</MbText>
-    <MbText className="big-p mb-2">big-p</MbText>
-    <MbText className="med-p-bold mb-2">med-p-bold</MbText>
-    <MbText className="med-p mb-2">med-p</MbText>
-    <MbText className="small-p-bold mb-2">small-p-bold</MbText>
-    <MbText className="small-p mb-2">small-p</MbText>
-    <MbText className="big-cap-bold mb-2">big-cap-bold</MbText>
-    <MbText className="big-cap mb-2">big-cap</MbText>
-    <MbText className="med-cap-bold mb-2">med-cap-bold</MbText>
-    <MbText className="med-cap mb-2">med-cap</MbText>
-    <MbText className="small-cap-bold mb-2">small-cap-bold</MbText>
-    <MbText className="small-cap mb-2">small-cap</MbText>
+    <MbText className="heading-130 mb-2">heading-130</MbText>
+    <MbText className="heading-90 mb-2">heading-90</MbText>
+    <MbText className="h1-130 mb-2">h1-130</MbText>
+    <MbText className="h1-90 mb-2">h1-90</MbText>
+    <MbText className="h2-130 mb-2">h2-130</MbText>
+    <MbText className="h2-90 mb-2">h2-90</MbText>
+    <MbText className="h3-130 mb-2">h3-130</MbText>
+    <MbText className="h3-90 mb-2">h3-90</MbText>
+    <MbText className="p-sub-130 mb-2">p-sub-130</MbText>
+    <MbText className="p-sub-90 mb-2">p-sub-90</MbText>
+    <MbText className="p-big-130 mb-2">p-big-130</MbText>
+    <MbText className="p-big-90 mb-2">p-big-90</MbText>
+    <MbText className="p-med-130 mb-2">p-med-130</MbText>
+    <MbText className="p-med-90 mb-2">p-med-90</MbText>
+    <MbText className="p-small-130 mb-2">p-small-130</MbText>
+    <MbText className="p-small-90 mb-2">p-small-90</MbText>
+    <MbText className="cap-big-130 mb-2">cap-big-130</MbText>
+    <MbText className="cap-big-90 mb-2">cap-big-90</MbText>
+    <MbText className="cap-med-130 mb-2">cap-med-130</MbText>
+    <MbText className="cap-med-90 mb-2">cap-med-90</MbText>
+    <MbText className="cap-small-130 mb-2">cap-small-130</MbText>
+    <MbText className="cap-small-90 mb-2">cap-small-90</MbText>
     <MbText className="action-cap mb-2">action-cap</MbText>
   </section>
 );
