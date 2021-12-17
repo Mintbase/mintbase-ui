a0bfc93e30e02355d2ba84d34308770beb63e825
Rui Santiago <ruisantiagomr@gmail.com>
2021-12-17 17:34:58 +0000

    feat: items per page component

--- src/components/navigation/itemsperpage/ItemsPerPage.tsx
+++ src/components/navigation/itemsperpage/ItemsPerPage.tsx
@@ -1,3 +1,26 @@
-const MbItemsPerPage = () => {};
+import "./itemsperpage.css";
+
+const MbItemsPerPage = ({
+  onItemsPage,
+  currentValue,
+}: {
+  onItemsPage: (page: number) => void;
+  currentValue: number;
+}) => {
+  const options = [10, 20, 30, 40, 50];
+  return (
+    <ul className="flex items-center space-x-12">
+      <li className="p-med-90 dark:text-white">Items per page</li>
+      {options.map((option) => (
+        <li
+          className={`item p-med-90 ${currentValue === option ? "active" : ""}`}
+          onClick={() => onItemsPage(option)}
+        >
+          {option}
+        </li>
+      ))}
+    </ul>
+  );
+};
 
 export default MbItemsPerPage;
---
+++ src/stories/components/ItemsPerPage.stories.tsx
@@ -0,0 +1,28 @@
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+import React, { useState } from "react";
+import MbItemsPerPage from "../../components/navigation/itemsperpage/ItemsPerPage";
+
+export default {
+  title: "Components/Navigation/Items",
+  component: MbItemsPerPage,
+  argTypes: {},
+} as ComponentMeta<typeof MbItemsPerPage>;
+
+const Template: ComponentStory<typeof MbItemsPerPage> = (args: any) => {
+  const [currentSetup, setCurrentSetUp] = useState(10);
+  const handle = (page: number) => {
+    setCurrentSetUp(page);
+  };
+  return (
+    <MbItemsPerPage
+      {...args}
+      onItemsPage={handle}
+      currentValue={currentSetup}
+    />
+  );
+};
+
+export const Items = Template.bind({});
+Items.args = {
+  currentValue: 10,
+};
