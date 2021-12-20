d420f5686e281eebaa1a0dd967185ceb73743083
Rui Santiago <ruisantiagomr@gmail.com>
2021-12-20 18:10:54 +0000

    wip: add text and colors classes

--- src/components/table/Table.tsx
+++ src/components/table/Table.tsx
@@ -8,15 +8,15 @@ const MbTable = ({
   data: any[];
 }) => {
   return (
-    <>
-      <header className="p-24 border-b border-gray-200 dark:border-gray-800">
+    <section className="bg-white dark:bg-gray-850">
+      <header className="p-24 border-b border-gray-200 dark:border-gray-800 dark:text-white p-big-130">
         {name}
       </header>
       <table className="w-full">
-        <thead className="table-fixed border-b-2 border-gray-200 text-left dark:border-gray-900">
+        <thead className="table-fixed border-b-2 border-gray-200 text-left dark:border-gray-900 text-gray-700 dark:text-gray-300">
           <tr>
             {headers.map((header) => (
-              <th className="pl-24 p-16">{header}</th>
+              <th className="pl-24 p-16 p-med-90">{header}</th>
             ))}
           </tr>
         </thead>
@@ -26,14 +26,16 @@ const MbTable = ({
               return (
                 <tr className="border-b border-gray-200 dark:border-gray-800">
                   {Object.values(elm).map((val: any) => {
-                    return <td className="p-24">{val}</td>;
+                    return (
+                      <td className="p-24 dark:text-white p-med-90">{val}</td>
+                    );
                   })}
                 </tr>
               );
             })}
         </tbody>
       </table>
-    </>
+    </section>
   );
 };
 
