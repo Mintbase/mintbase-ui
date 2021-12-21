9ef8e58571ba5f2c566a0be372a133f010d1573d
Rui Santiago <ruisantiagomr@gmail.com>
2021-12-21 12:23:29 +0000

    wip: changed stories

--- src/stories/components/Table.stories.tsx
+++ src/stories/components/Table.stories.tsx
@@ -1,5 +1,6 @@
 import { ComponentMeta, ComponentStory } from "@storybook/react";
-import MbTable from "../../components/table/Table";
+import { useRef } from "react";
+import MbTable, { MbBody, MbHead } from "../../components/table/Table";
 
 export default {
   title: "Components/Table",
@@ -9,42 +10,72 @@ export default {
 
 const Template: ComponentStory<typeof MbTable> = (args) => (
   <MbTable {...args}>
-    <p>This is the description.</p>
+    <MbHead>
+      <tr id="headers">
+        <td>Offer</td>
+        <td>From</td>
+        <td>Created</td>
+        <td>Expires</td>
+        <td>TX</td>
+      </tr>
+    </MbHead>
+    <MbBody>
+      <tr id="headers">
+        <td>557.6 N</td>
+        <td>mintbase.near</td>
+        <td>1 hour ago</td>
+        <td>1 hour ago</td>
+        <td>...</td>
+      </tr>
+      <tr id="headers">
+        <td>222 N</td>
+        <td>mintbase.near</td>
+        <td>1 hour ago</td>
+        <td>3 hours ago</td>
+        <td>...</td>
+      </tr>
+      <tr id="headers">
+        <td>104,7 N</td>
+        <td>mintbase.near</td>
+        <td>1 hour ago</td>
+        <td>1 hour ago</td>
+        <td>...</td>
+      </tr>
+    </MbBody>
   </MbTable>
 );
 
 export const Table = Template.bind({});
 Table.args = {
-  name: "Trading History",
-  headers: ["Offer", "From", "Created", "Expires", "TX"],
-  data: [
-    {
-      amount: "557.6 N",
-      account: "mintbase.near",
-      created: "1 hour ago",
-      expires: "1 hour ago",
-      tx: "...",
-    },
-    {
-      amount: "222 N",
-      account: "mintbase.near",
-      created: "3 hours ago",
-      expires: "4 hours ago",
-      tx: "...",
-    },
-    {
-      amount: "312,67 N",
-      account: "mintbase.near",
-      created: "10 hours ago",
-      expires: "1 hour ago",
-      tx: "...",
-    },
-    {
-      amount: "10.6 N",
-      account: "mintbase.near",
-      created: "1 hour ago",
-      expires: "1 hour ago",
-      tx: "...",
-    },
-  ],
+  title: "Trading History",
+  // data: [
+  //   {
+  //     amount: "557.6 N",
+  //     account: "mintbase.near",
+  //     created: "1 hour ago",
+  //     expires: "1 hour ago",
+  //     tx: "...",
+  //   },
+  //   {
+  //     amount: "222 N",
+  //     account: "mintbase.near",
+  //     created: "3 hours ago",
+  //     expires: "4 hours ago",
+  //     tx: "...",
+  //   },
+  //   {
+  //     amount: "312,67 N",
+  //     account: "mintbase.near",
+  //     created: "10 hours ago",
+  //     expires: "1 hour ago",
+  //     tx: "...",
+  //   },
+  //   {
+  //     amount: "10.6 N",
+  //     account: "mintbase.near",
+  //     created: "1 hour ago",
+  //     expires: "1 hour ago",
+  //     tx: "...",
+  //   },
+  // ],
 };
