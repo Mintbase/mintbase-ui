6e66b08d26532056a2803aea6bc6a8b798f74703
Rui Santiago <ruisantiagomr@gmail.com>
2021-12-21 16:33:13 +0000

    feat: table component

--- src/components/table/Table.tsx
+++ src/components/table/Table.tsx
@@ -1,7 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
+import MbItemsPerPage from "../navigation/itemsperpage/ItemsPerPage";
+import MbPagination, {
+  PaginationProps,
+} from "../navigation/pagination/Pagination";
 import "./table.css";
 interface TableProps extends React.HTMLAttributes<HTMLDivElement> {
   title: string;
+  pagination?: PaginationProps;
+  onAmountItemsChange?: (page: number) => void;
 }
 
 export const MbHead = (props: React.HTMLAttributes<HTMLDivElement>) => {
@@ -21,11 +27,12 @@ export const MbBody = (props: React.HTMLAttributes<HTMLDivElement>) => {
 };
 
 const MbTable = (props: TableProps) => {
-  const { title, children } = props;
+  const { title, children, pagination, onAmountItemsChange } = props;
 
   const allChildren = React.Children.map(children, (child: any) => child);
   const headers = allChildren?.filter((child) => child.type.name === "MbHead");
   const body = allChildren?.filter((child) => child.type.name === "MbBody");
+  console.log(body);
 
   return (
     <section className="bg-white dark:bg-gray-850">
@@ -36,6 +43,34 @@ const MbTable = (props: TableProps) => {
         {headers}
         {body}
       </table>
+      {pagination && (
+        <footer className="border-t border-gray-200 dark:border-gray-800 p-24 grid md:grid-cols-3 items-center">
+          {onAmountItemsChange && (
+            <div className="order-2 md:order-1 flex justify-center md:justify-start">
+              <MbItemsPerPage
+                currentValue={pagination.itemsPerPage}
+                onItemsPage={(nr) => onAmountItemsChange(nr)}
+              />
+            </div>
+          )}
+
+          <div className="p-med-90 dark:text-white text-center hidden md:block order-2">
+            {pagination.itemsPerPage * pagination.currentPage <
+            pagination.totalItems
+              ? `1 - ${pagination.itemsPerPage}`
+              : pagination.totalItems}{" "}
+            of {pagination.totalItems}
+          </div>
+          <div className="flex justify-center md:justify-end mb-24 md:mb-0 order-1 md:order-3">
+            <MbPagination
+              currentPage={pagination.currentPage}
+              itemsPerPage={pagination.itemsPerPage}
+              totalItems={pagination.totalItems}
+              onPageChange={pagination.onPageChange}
+            />
+          </div>
+        </footer>
+      )}
     </section>
   );
 };
