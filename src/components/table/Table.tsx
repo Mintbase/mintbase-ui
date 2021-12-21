92fff376b99fb96a77cf3a56a1ae9149c3c5edb4
Rui Santiago <ruisantiagomr@gmail.com>
2021-12-21 12:23:44 +0000

    wip: change component logic to receive full table as children

--- src/components/table/Table.tsx
+++ src/components/table/Table.tsx
@@ -1,39 +1,40 @@
-const MbTable = ({
-  name,
-  headers,
-  data,
-}: {
-  name: string;
-  headers: string[];
-  data: any[];
-}) => {
+import React from "react";
+import "./table.css";
+interface TableProps extends React.HTMLAttributes<HTMLDivElement> {
+  title: string;
+}
+
+export const MbHead = (props: React.HTMLAttributes<HTMLDivElement>) => {
+  return (
+    <thead className="headers table-fixed border-b-2 border-gray-200 text-left dark:border-gray-900 text-gray-700 dark:text-gray-300 p-med-90">
+      {props.children}
+    </thead>
+  );
+};
+
+export const MbBody = (props: React.HTMLAttributes<HTMLDivElement>) => {
+  return (
+    <tbody className="p-24 dark:text-white p-med-90 body-table">
+      {props.children}
+    </tbody>
+  );
+};
+
+const MbTable = (props: TableProps) => {
+  const { title, children } = props;
+
+  const allChildren = React.Children.map(children, (child: any) => child);
+  const headers = allChildren?.filter((child) => child.type.name === "MbHead");
+  const body = allChildren?.filter((child) => child.type.name === "MbBody");
+
   return (
     <section className="bg-white dark:bg-gray-850">
       <header className="p-24 border-b border-gray-200 dark:border-gray-800 dark:text-white p-big-130">
-        {name}
+        {title}
       </header>
       <table className="w-full">
-        <thead className="table-fixed border-b-2 border-gray-200 text-left dark:border-gray-900 text-gray-700 dark:text-gray-300">
-          <tr>
-            {headers.map((header) => (
-              <th className="pl-24 p-16 p-med-90">{header}</th>
-            ))}
-          </tr>
-        </thead>
-        <tbody>
-          {data &&
-            data.map((elm, i) => {
-              return (
-                <tr className="border-b border-gray-200 dark:border-gray-800">
-                  {Object.values(elm).map((val: any) => {
-                    return (
-                      <td className="p-24 dark:text-white p-med-90">{val}</td>
-                    );
-                  })}
-                </tr>
-              );
-            })}
-        </tbody>
+        {headers}
+        {body}
       </table>
     </section>
   );
