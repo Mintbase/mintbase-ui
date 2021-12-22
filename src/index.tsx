a2b195f4c6799fe5aaea7f753bdbead0a1daa157
João Loureiro <loureiro.jpc@gmail.com>
2021-12-22 16:18:04 +0000

    build: add static files

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import Accordion from './components/accordion/Accordion'
 import Action from './components/action/Action'
 import Button from './components/button/Button'
@@ -8,15 +7,6 @@ import ThingCard from './components/cards/thing/ThingCard'
 import Icon from './components/icon/Icon'
 import InputField from './components/input-fields/inputField'
 import Tooltip from './components/tooltip/Tooltip'
-
-export const Thing: React.FC = ({ children }) => {
-    return (
-      // We are adding these classes here
-      <div className="mt-10 bg-yellow-100 p-5 rounded-md w-24">
-        {children || `the snozzberries taste like snozzberries`}
-      </div>
-    );
-};
 
 export {
   Accordion,
