f8b13473b16b053d42d0ae33a2c43c9d941a58fd
Rui Santiago <ruisantiagomr@gmail.com>
2021-12-23 00:33:19 +0000

    remove clogs

--- src/constants/mobile.ts
+++ src/constants/mobile.ts
@@ -1,8 +1,6 @@
 import tailwindConfig from "../../tailwind.config";
 
 export const isMobile = () => {
-  console.log(window.innerWidth);
-  console.log(Number(tailwindConfig.theme.screens.sm.replace("px", "")));
   return (
     window.innerWidth <
     Number(tailwindConfig.theme.screens.sm.replace("px", ""))
