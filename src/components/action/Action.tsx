c26706f2bb70da8d71896bee3300dbe65c487ef5
Rui Santiago <ruisantiagomr@gmail.com>
2021-12-27 15:39:09 +0000

    wip: add button props to action

--- src/components/action/Action.tsx
+++ src/components/action/Action.tsx
@@ -2,11 +2,11 @@ import { getFontType } from "../../constants/fontType";
 import { ESize, EState } from "../../constants/properties";
 import "./action.css";
 
-type ActionProps = {
+interface ActionProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   label: string;
   state?: EState.ACTIVE | EState.DISABLED;
   size?: ESize;
-};
+}
 
 const MbAction = ({
   label = "",
