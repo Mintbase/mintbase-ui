73b8d48695118bf5c35b485d90e2b112595e1e42
Rui Santiago <ruisantiagomr@gmail.com>
2021-12-27 15:37:08 +0000

    wip: add onclick actions

--- src/components/tabs/Tabs.tsx
+++ src/components/tabs/Tabs.tsx
@@ -8,14 +8,16 @@ interface TabsProps extends React.HTMLAttributes<HTMLDivElement> {
   hasFilters: boolean;
 }
 
-const OrderOptions = () => {
+const OrderOptions = ({ onSelect }: { onSelect: (option: string) => void }) => {
+  const labels = ["Newest", "Oldest", "Cheapest", "Most expensive"];
   return (
     <>
       <div className="bg-gray-50 dark:bg-gray-800 rounded-b absolute flex flex-col top-12 py-12 w-max center-sub-menu">
-        <MbAction label="Newest" />
-        <MbAction label="Oldest" />
-        <MbAction label="Cheapest" />
-        <MbAction label="Most expensive" />
+        {labels.map((label) => (
+          <div onClick={() => onSelect(label)}>
+            <MbAction label={label} />
+          </div>
+        ))}
       </div>
     </>
   );
@@ -24,6 +26,7 @@ const OrderOptions = () => {
 const MbTabs = (props: TabsProps) => {
   const [selectedTab, setSelectedTab] = useState(0);
   const [showOrderOpts, setShowOrderOpts] = useState(false);
+  const [selectedOrder, setSelectedOrder] = useState("");
 
   if (!props.children) return <></>;
   const allTabs = React.Children.map(props.children, (child: any) => child);
@@ -47,10 +50,25 @@ const MbTabs = (props: TabsProps) => {
         {props.hasFilters && (
           <>
             <div className="ml-auto relative">
-              <div className="hover:bg-blue-300-15 dark:hover:bg-blue-100-35 cursor-pointer rounded-full">
-                <div className="flex p-16">
-                  <div className="text-blue-300 dark:text-blue-100 p-med-90 pr-10">
-                    Order By
+              <div
+                className={`${
+                  selectedOrder
+                    ? "bg-mb-red-15 dark:bg-mb-red-35"
+                    : "hover:bg-blue-300-15 dark:hover:bg-blue-100-35"
+                } cursor-pointer rounded-full`}
+              >
+                <div
+                  className="flex p-16"
+                  onClick={() => setShowOrderOpts(!showOrderOpts)}
+                >
+                  <div
+                    className={`${
+                      selectedOrder
+                        ? "text-mb-red"
+                        : "text-blue-300 dark:text-blue-100"
+                    } p-med-90 pr-10`}
+                  >
+                    {selectedOrder ? selectedOrder : "Order By"}
                   </div>
                   <MbIcon
                     name={EIconName.ARROW_DROP_DOWN}
@@ -60,7 +78,14 @@ const MbTabs = (props: TabsProps) => {
                   />
                 </div>
               </div>
-              {showOrderOpts && <OrderOptions />}
+              {showOrderOpts && (
+                <OrderOptions
+                  onSelect={(option) => {
+                    setShowOrderOpts(!showOrderOpts);
+                    setSelectedOrder(option);
+                  }}
+                />
+              )}
             </div>
           </>
         )}
