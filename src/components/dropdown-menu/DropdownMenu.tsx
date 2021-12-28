e7b41879a9789b25f827101f1dcecb8972903c52
Rui Santiago <ruisantiagomr@gmail.com>
2021-12-28 15:03:46 +0000

    last fixes in the dropwdownmenu

--- src/components/dropdown-menu/DropdownMenu.tsx
+++ src/components/dropdown-menu/DropdownMenu.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { MbText } from "../text/Text";
+import "./dropdownmenu.css";
 
 interface Item {
   text: string;
@@ -19,7 +20,7 @@ export const MbDropdownMenu = ({
 }) => {
   return !isOpen ? null : (
     <div
-      className={`bg-gray-50 dark:bg-gray-700 text-mb-blackblue dark:text-white shadow-md rounded-md w-64 mt-4 overflow-hidden ${className}`}
+      className={`bg-gray-50 dark:bg-gray-800 text-mb-blackblue dark:text-white shadow-md rounded-md w-64 mt-4 overflow-hidden ${className}`}
     >
       {items.map(({ text, icon, onClick }, index) => {
         // TODO: fix over rounded corners for the first and last element
@@ -27,7 +28,7 @@ export const MbDropdownMenu = ({
           <div
             key={`${text}-${index}`}
             className={
-              "flex hover:bg-blue-300-15 dark:hover:bg-blue-100-35 p-8 cursor-pointer"
+              "flex hover:bg-blue-300-15 dark:hover:bg-blue-100-35 p-8 cursor-pointer relative"
             }
             onClick={onClick}
           >
@@ -38,7 +39,7 @@ export const MbDropdownMenu = ({
             >
               {text}
             </MbText>
-            {icon && <div className={"flex items-center ml-4 w-6"}>{icon}</div>}
+            {icon && <div className={"dropdown-icon"}>{icon}</div>}
           </div>
         );
       })}
