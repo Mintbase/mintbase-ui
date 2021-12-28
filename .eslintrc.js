0965d1a6771f32b48607dcc9852fb65d47e772c5
João Loureiro <loureiro.jpc@gmail.com>
2021-12-28 15:41:21 +0000

    update lint configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,8 @@
 module.exports = {
-  "root": true,
+  "env": {
+    "browser": true,
+    "node": true,
+  },
   "parser": "@typescript-eslint/parser",
   "plugins": [
     "@typescript-eslint"
--- .lintstagedrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  // Lint then format Typescript and Javascript files
-  '**/*.(ts|tsx)': (filenames) => [
-    `next lint --fix --file ${filenames
-      .map((file) => file.split(process.cwd())[1])
-      .join(' --file ')}`,
-    `prettier --write ${filenames.join(' ')}`,
-  ],
-}
