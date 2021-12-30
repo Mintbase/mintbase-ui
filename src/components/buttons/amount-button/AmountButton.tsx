23d34143386bba45de39bb40cbbc0d1c720a5060
Rui Santiago <ruisantiagomr@gmail.com>
2021-12-30 14:56:48 +0000

    wip base  code for the amount

--- src/components/buttons/amount-button/AmountButton.tsx
+++ src/components/buttons/amount-button/AmountButton.tsx
@@ -1,5 +1,15 @@
 const MbAmountButton = () => {
-  return <></>
+  return (
+    <div className="flex items-center space-x-24">
+      <div className="p-big-90 bg-black rounded text-white w-8 h-8 flex justify-center items-center cursor-pointer">
+        -
+      </div>
+      <div className="h2-90">1</div>
+      <div className="p-big-90 bg-black rounded text-white w-8 h-8 flex justify-center items-center cursor-pointer">
+        +
+      </div>
+    </div>
+  )
 }
 
 export default MbAmountButton
