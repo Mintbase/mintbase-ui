daf778b20fb800519f8601be101b00c4ff2fdf42
João Loureiro <loureiro.jpc@gmail.com>
2021-12-31 10:59:56 +0000

    refactor(MbIcon): use multiple conditional rendering
    this allows better variant handling

--- src/components/icon/Icon.tsx
+++ src/components/icon/Icon.tsx
@@ -1,6 +1,12 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import InlineSVG from 'react-inlinesvg'
 import { EIconName } from '../../consts/icons'
+
+enum IconType {
+  MINTBASE = 'mintbase',
+  CIRCLE = 'circle',
+  MATERIAL = 'material',
+}
 
 const mintbaseIcons = [
   'facebook',
@@ -19,7 +25,7 @@ export const MbIcon = ({
   color = 'mb-blackblue',
   darkColor,
   size = '24px',
-  className=''
+  className = '',
 }: {
   name: EIconName
   color: string
@@ -27,33 +33,40 @@ export const MbIcon = ({
   size?: string
   className?: string
 }) => {
-  const [isMintbaseIcon, setIsMintbaseIcon] = useState<boolean>(false)
-
-  useEffect(() => {
-    checkIfMaterialIcon()
-  }, [])
-
-  const checkIfMaterialIcon = () => {
-    setIsMintbaseIcon(mintbaseIcons.includes(name))
+  const iconType = () => {
+    if (mintbaseIcons.includes(name)) return IconType.MINTBASE
+    if (name === EIconName.CIRCLE) return IconType.CIRCLE
+    return IconType.MATERIAL
   }
 
+  // TODO: remove dynamic classNames once we start using tailwindcss purge feature
   return (
     <div className={`flex justify-center ${className}`}>
-      {isMintbaseIcon ? (
-        <InlineSVG
-          src={`/assets/icons/${name}.svg`}
-          width={size}
-          height={size}
-          className={`fill-current text-${color} dark:text-${darkColor}`}
-        />
-      ) : (
-        <span
-          className={`material-icons text-${color} dark:text-${darkColor}`}
-          style={{ fontSize: size }}
-        >
-          {name}
-        </span>
-      )}
+      {
+        {
+          [IconType.MINTBASE]: (
+            <InlineSVG
+              src={`/assets/icons/${name}.svg`}
+              width={size}
+              height={size}
+              className={`fill-current text-${color} dark:text-${darkColor}`}
+            />
+          ),
+          [IconType.MATERIAL]: (
+            <span
+              className={`material-icons text-${color} dark:text-${darkColor}`}
+              style={{ fontSize: size }}
+            >
+              {name}
+            </span>
+          ),
+          [IconType.CIRCLE]: (
+            <div
+              className={`w-3 h-3 rounded-full bg-${color} dark:bg-${darkColor}`}
+            />
+          ),
+        }[iconType()]
+      }
     </div>
   )
 }
