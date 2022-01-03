5e17bde09e8f0a156bc72f169cd4921c8575f0f7
Rui Santiago <ruisantiagomr@gmail.com>
2022-01-03 15:00:35 +0000

    add props to modal

--- src/components/modal/Modal.tsx
+++ src/components/modal/Modal.tsx
@@ -1,5 +1,14 @@
-const MbModal = () => {
-  return <></>
+interface ModalProps extends React.HTMLAttributes<HTMLDivElement> {
+  open: boolean
+  onClose: () => void
+}
+const MbModal = (props: ModalProps) => {
+  const { open, onClose, children } = props
+  return (
+    <>
+      {open && <div className="bg-white dark:bg-gray-850 p-24">{children}</div>}
+    </>
+  )
 }
 
 export default MbModal
