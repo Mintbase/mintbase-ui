2537a4a702c9b96f1f55c47dd00fee503e42ee08
Rui Santiago <ruisantiagomr@gmail.com>
2022-01-03 15:29:56 +0000

    wip: add modal on click and title

--- src/components/modal/Modal.tsx
+++ src/components/modal/Modal.tsx
@@ -1,12 +1,20 @@
 interface ModalProps extends React.HTMLAttributes<HTMLDivElement> {
+  title: string
   open: boolean
   onClose: () => void
 }
 const MbModal = (props: ModalProps) => {
-  const { open, onClose, children } = props
+  const { open, onClose, title, children } = props
   return (
     <>
-      {open && <div className="bg-white dark:bg-gray-850 p-24">{children}</div>}
+      {open && (
+        <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+          <div className="relative w-auto my-6 mx-auto max-w-3xl bg-white dark:bg-gray-850 rounded">
+            <header className="flex items-center p-32">{title}</header>
+            {children}
+          </div>{' '}
+        </div>
+      )}
     </>
   )
 }
---
+++ src/stories/components/Modal.stories.tsx
@@ -0,0 +1,32 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react'
+import { useState } from 'react'
+import { ESize, EState, EType } from '../../consts/properties'
+import { MbButton } from './../../components/buttons/button/Button'
+import MbModal from './../../components/modal/Modal'
+
+export default {
+  title: 'Components/Modal',
+  component: MbModal,
+  argTypes: {},
+} as ComponentMeta<typeof MbModal>
+
+const Template: ComponentStory<typeof MbModal> = (args) => {
+  const [open, setIsOpen] = useState(false)
+  return (
+    <>
+      <MbButton
+        label="Open Modal"
+        state={EState.ACTIVE}
+        btnType={EType.PRIMARY}
+        size={ESize.MEDIUM}
+        onClick={() => setIsOpen(true)}
+      />
+      <MbModal {...args} open={open} title="Info">
+        <p>This is a modal content example.</p>
+      </MbModal>
+    </>
+  )
+}
+
+export const Modal = Template.bind({})
+Modal.args = {}
