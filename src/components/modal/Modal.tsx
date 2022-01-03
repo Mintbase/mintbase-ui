8e167c425fc3a4cf920bf08be3a1a5c1e8446b9e
Rui Santiago <ruisantiagomr@gmail.com>
2022-01-03 16:20:35 +0000

    add overlay bg for modal

--- src/components/modal/Modal.tsx
+++ src/components/modal/Modal.tsx
@@ -12,23 +12,30 @@ const MbModal = (props: ModalProps) => {
   return (
     <>
       {open && (
-        <div className="modal">
-          <section className="modal-section">
-            <header className="flex items-center p-24 border-b border-gray-150 justify-between">
-              <div className="p-big-130">{title}</div>
-              <div onClick={onClose}>
-                <MbIcon
-                  name={EIconName.CLOSE}
-                  size="20px"
-                  color="blue-300"
-                  darkColor="blue-100"
-                  className="cursor-pointer"
-                />
-              </div>
-            </header>
-            <div className="p-24">{children}</div>
-          </section>
-        </div>
+        <>
+          <div className="fixed inset-0 bg-gray-150 dark:border-gray-700 bg-opacity-50 overflow-y-auto h-full w-full"></div>
+          <div
+            className={`modal transition-all ease-in-out duration-500 ${
+              open ? 'opacity-100' : 'opacity-0'
+            }`}
+          >
+            <section className="modal-section">
+              <header className="flex items-center p-24 border-b border-gray-150 dark:border-gray-700 justify-between">
+                <div className="p-big-130">{title}</div>
+                <div onClick={onClose}>
+                  <MbIcon
+                    name={EIconName.CLOSE}
+                    size="20px"
+                    color="blue-300"
+                    darkColor="blue-100"
+                    className="cursor-pointer"
+                  />
+                </div>
+              </header>
+              <div className="p-24">{children}</div>
+            </section>
+          </div>
+        </>
       )}
     </>
   )
