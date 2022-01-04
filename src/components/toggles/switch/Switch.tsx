b149bc5925dbad4deda58d63a54224281f9a567a
Rui Santiago <ruisantiagomr@gmail.com>
2022-01-04 13:00:13 +0000

    wip: add props to switch component

--- src/components/toggles/switch/Switch.tsx
+++ src/components/toggles/switch/Switch.tsx
@@ -1,15 +1,23 @@
 import './switch.css'
 
-const MbSwitch = () => {
+interface SwitchProps {
+  id: string
+  isChecked: boolean
+  handleChange: (checked: boolean) => void
+}
+
+const MbSwitch = (props: SwitchProps) => {
+  const { id, isChecked, handleChange } = props
   return (
     <div className="switch">
       <input
         type="checkbox"
         className="switch-checkbox"
-        name="toggleSwitch"
-        id="toggleSwitch"
+        id={id}
+        checked={isChecked}
+        onChange={(e) => handleChange(e.target.checked)}
       />
-      <label className="switch-label" htmlFor="toggleSwitch">
+      <label className="switch-label" htmlFor={id}>
         <span className="switch-btn" />
       </label>
     </div>
