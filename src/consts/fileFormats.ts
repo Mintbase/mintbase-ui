574fb1741a4af4ca7a9113bd364a31eb0165e893
Rui Santiago <ruisantiagomr@gmail.com>
2022-01-05 16:02:05 +0000

    rm comment

--- src/consts/fileFormats.ts
+++ src/consts/fileFormats.ts
@@ -22,22 +22,3 @@ export const VALID_FILE_FORMATS = {
   ],
   [EMediaType.DOCUMENT]: ['application/pdf'],
 }
-
-// export const VALID_FILE_FORMATS: { [key: string]: string[] } = {
-//     [MetadataField.Media]: ['image/jpeg', 'image/png', 'image/gif'],
-//     [MetadataField.Animation_url]: [
-//       'image/png',
-//       'image/jpeg',
-//       'image/gif',
-//       'image/svg',
-//       'audio/ogg',
-//       'video/webm',
-//       'video/mp4',
-//       'audio/mpeg',
-//       'audio/mp3',
-//       'model/gltf-binary',
-//       'model/gltf+json',
-//       'application/octet-stream',
-//     ],
-//     [MetadataField.Document]: ['application/pdf'],
-//   }
