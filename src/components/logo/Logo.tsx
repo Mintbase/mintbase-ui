da6698f6280a169f29f670441ffad6c15e8bf889
Rui Santiago <ruisantiagomr@gmail.com>
2022-01-06 17:02:55 +0000

    Update Logo.tsx

--- src/components/logo/Logo.tsx
+++ src/components/logo/Logo.tsx
@@ -4,20 +4,17 @@ import InlineSVG from 'react-inlinesvg/esm'
 interface LogoProps {
   src: string
   width?: string
-  height?: string
   className?: string
 }
 export const MbLogo = ({
   src,
   width = '128px',
-  height = '32px',
   className = '',
 }: LogoProps) => (
   <div className={className}>
     <InlineSVG
       src={src}
       width={width}
-      height={height}
       className={`fill-current text-black dark:text-white`}
     />
   </div>
