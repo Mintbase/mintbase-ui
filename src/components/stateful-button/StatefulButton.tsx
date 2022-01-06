d4c5e4acc8f02f13b40cbba9f4330ae327f33073
João Loureiro <1649425+jpcloureiro@users.noreply.github.com>
2022-01-06 10:46:02 +0000

    statefulButton: add pointer cursor (#74)
    Co-authored-by: Rui Santiago <ruisantiagomr@gmail.com>

---
+++ src/components/stateful-button/StatefulButton.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+export interface MbStatefulButtonProps {
+  indicator: JSX.Element
+  content: JSX.Element
+  className?: string
+  onClick?: React.MouseEventHandler<HTMLDivElement> 
+}
+
+export const MbStatefulButton = ({
+  indicator,
+  content,
+  className = '',
+  onClick,
+}: MbStatefulButtonProps) => {
+  return (
+    <div
+      className={`flex p-12 h-12 justify-center dark:text-white items-center rounded-md bg-gray-100 dark:hover:bg-gray-850 dark:bg-gray-800 select-none cursor-pointer ${className}`}
+      onClick={onClick}
+    >
+      {indicator}
+      {content}
+    </div>
+  )
+}
