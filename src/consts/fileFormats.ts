1288811053d22b2dccbaf292ffcd93917a1f3950
Rui Santiago <ruisantiagomr@gmail.com>
2022-01-12 17:15:46 +0000

    add types

--- src/consts/fileFormats.ts
+++ src/consts/fileFormats.ts
@@ -22,3 +22,18 @@ export const VALID_FILE_FORMATS = {
   ],
   [EMediaType.DOCUMENT]: ['application/pdf'],
 }
+
+export const AUDIO_TYPES = ['audio/ogg', 'audio/mpeg', 'audio/mp3']
+export const VIDEO_TYPES = ['video/webm', 'video/mp4']
+export const IMAGE_TYPES = [
+  'image/png',
+  'image/jpeg',
+  'image/gif',
+  'image/svg+xml',
+]
+export const THREED_TYPES = [
+  'model/gltf-binary',
+  'model/gltf+json',
+  'application/octet-stream',
+]
+export const FILE_TYPES = ['application/pdf']
