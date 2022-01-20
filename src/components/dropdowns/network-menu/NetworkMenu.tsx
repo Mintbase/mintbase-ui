47e80d902e6cfeca23213859eae89da558ea0101
Rui Santiago <ruisantiagomr@gmail.com>
2022-01-20 18:39:30 +0000

    add col and row display

--- src/components/dropdowns/network-menu/NetworkMenu.tsx
+++ src/components/dropdowns/network-menu/NetworkMenu.tsx
@@ -5,14 +5,15 @@ import { TNetworkOption } from '../../../types/network.type'
 interface NetworkMenuProps {
   options: TNetworkOption[]
   isOpen: boolean
+  direction: 'col' | 'row'
   onOptionChange: (selected: string) => void
 }
 
 export const MbNetworkMenu = (props: NetworkMenuProps) => {
-  const { options, isOpen, onOptionChange } = props
+  const { options, isOpen, direction = 'col', onOptionChange } = props
   if (!isOpen) return null
   return (
-    <div className="network-menu">
+    <div className={`network-menu ${direction}`}>
       {options.map((option) => (
         <div className="network-item">
           <div className={`w-3 h-3 rounded-full ${option.indicatorColor}`} />
