8f0fb250c4c574744f5f4cb640241354dab941da
Rui Santiago <ruisantiagomr@gmail.com>
2022-01-20 13:54:25 +0000

    add network menu logic

--- src/components/dropdowns/network-menu/NetworkMenu.tsx
+++ src/components/dropdowns/network-menu/NetworkMenu.tsx
@@ -1,7 +1,28 @@
 import './networkmenu.css'
+import { MbText } from '../../text/Text'
 
-const MbNetworkMenu = () => {
-  return <div className="network-menu"></div>
+interface NetworkMenuProps {
+  onOptionChange: (selected: string) => void
+}
+
+const MbNetworkMenu = (props: NetworkMenuProps) => {
+  const { onOptionChange } = props
+  return (
+    <div className="network-menu">
+      {[
+        { label: 'NEAR TESTNET', value: 'testnet' },
+        { label: 'NEAR MAINNET', value: 'mainnet' },
+        { label: 'ETHEREUM', value: 'ethereum' },
+      ].map((network) => (
+        <MbText
+          className={'ml-8 p-small-90 align-center tracking-wider'}
+          onClick={() => onOptionChange(network.value)}
+        >
+          {network.label}
+        </MbText>
+      ))}
+    </div>
+  )
 }
 
 export default MbNetworkMenu
