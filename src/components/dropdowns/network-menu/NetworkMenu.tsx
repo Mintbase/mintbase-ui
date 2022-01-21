47fb6d6f22506351107ff05ebfe88213c1c3107a
Rui Santiago <ruisantiagomr@gmail.com>
2022-01-21 15:44:29 +0000

    removed row direction and added classes for menu and for item

--- src/components/dropdowns/network-menu/NetworkMenu.tsx
+++ src/components/dropdowns/network-menu/NetworkMenu.tsx
@@ -5,18 +5,20 @@ import { TNetworkOption } from '../../../types/network.type'
 interface NetworkMenuProps {
   options: TNetworkOption[]
   isOpen: boolean
-  direction: 'col' | 'row'
+  menuClassName?: string
+  itemClassName?: string
   onOptionChange: (selected: string) => void
 }
 
 export const MbNetworkMenu = (props: NetworkMenuProps) => {
-  const { options, isOpen, direction = 'col', onOptionChange } = props
+  const { options, isOpen, menuClassName, itemClassName, onOptionChange } =
+    props
   if (!isOpen) return null
   return (
-    <div className={`network-menu ${direction}`}>
+    <div className={`network-menu ${menuClassName}`}>
       {options.map((option) => (
         <div
-          className="network-item"
+          className={`network-item ${itemClassName}`}
           onClick={() => onOptionChange(option.value)}
           key={option.value}
         >
