96c3236f4abdd660c60e914ff57b9ad8a4fa2200
Rui Santiago <ruisantiagomr@gmail.com>
2022-01-25 17:22:23 +0000

    add extrafilter and props object to tab

--- src/components/tabs/Tab.tsx
+++ src/components/tabs/Tab.tsx
@@ -3,14 +3,16 @@ import './tab.css'
 
 interface TabProps extends React.HTMLAttributes<HTMLDivElement> {
   isActive?: boolean
+  extraFilter?: string
 }
 
 export const MbTab = (props: TabProps) => {
+  const { title, isActive } = props
   return (
     <>
       <div className="flex">
-        <div className={`tab p-med-90 ${props.isActive ? 'active' : ''}`}>
-          {props.title}
+        <div className={`tab p-med-90 ${isActive ? 'active' : ''}`}>
+          {title}
         </div>
       </div>
     </>
