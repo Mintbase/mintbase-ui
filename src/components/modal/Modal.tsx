6491dfb57129e1c628a224c169a293cccf97f7a0
Rui Santiago <ruisantiagomr@gmail.com>
2022-01-26 15:55:00 +0000

    change background color

--- src/components/modal/Modal.tsx
+++ src/components/modal/Modal.tsx
@@ -13,7 +13,7 @@ export const MbModal = (props: ModalProps) => {
     <>
       {open && (
         <>
-          <div className="fixed inset-0 bg-gray-150 dark:border-gray-700 bg-opacity-50 overflow-y-auto h-full w-full"></div>
+          <div className="fixed inset-0 bg-black dark:border-gray-700 bg-opacity-75 overflow-y-auto h-full w-full"></div>
           <div className="modal">
             <section className="modal-section">
               <header className="flex items-center p-24 border-b border-gray-150 dark:border-gray-700 justify-between">
