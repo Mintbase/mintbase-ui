9b6a129aef9a8c1710a3c52fb9233b1fc48a909c
Rui Santiago <ruisantiagomr@gmail.com>
2022-01-26 11:57:21 +0000

    add letter spacing widest to menu options

--- src/components/dropdowns/network-menu/NetworkMenu.tsx
+++ src/components/dropdowns/network-menu/NetworkMenu.tsx
@@ -35,7 +35,7 @@ export const MbNetworkMenu = (props: NetworkMenuProps) => {
           key={option.value}
         >
           <div className={`w-3 h-3 rounded-full ${option.indicatorColor}`} />
-          <MbText className={'p-small-90 align-center tracking-wider'}>
+          <MbText className={'p-small-90 align-center tracking-widest'}>
             {option.label}
           </MbText>
         </div>
