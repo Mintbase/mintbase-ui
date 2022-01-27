13a982def06c5e226bcba3987bb3e8b22e027f6f
Rui Santiago <ruisantiagomr@gmail.com>
2022-01-27 16:15:40 +0000

    add outside click function for dropdowns

---
+++ src/components/dropdowns/OutsideClickFunction.ts
@@ -0,0 +1,23 @@
+export default function listenForOutsideClicks(
+  listening: boolean,
+  setListening: React.Dispatch<React.SetStateAction<boolean>>,
+  menuRef: any,
+  setIsOpen: React.Dispatch<React.SetStateAction<boolean>>
+) {
+  return () => {
+    if (menuRef) {
+      if (listening) return
+      if (!menuRef.current) return
+      setListening(true)
+      ;[`click`, `touchstart`].forEach((type) => {
+        document.addEventListener(`click`, (evt) => {
+          const cur = menuRef.current
+          const node = evt.target
+          if (cur.contains(node)) return
+          setIsOpen(false)
+        })
+      })
+    }
+    return
+  }
+}
