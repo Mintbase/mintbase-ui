7ca7580c7000c0bb5faf264819d8aed5d18aef6a
Rui Santiago <ruisantiagomr@gmail.com>
2022-01-27 17:13:07 +0000

    fix outsideclick function

--- src/components/dropdowns/OutsideClickFunction.ts
+++ src/components/dropdowns/OutsideClickFunction.ts
@@ -6,17 +6,20 @@ export default function listenForOutsideClicks(
 ) {
   return () => {
     if (menuRef !== null) {
-      console.log(menuRef)
       if (listening) return
       if (!menuRef.current) return
       setListening(true)
       ;[`click`, `touchstart`].forEach((type) => {
-        document.addEventListener(`click`, (evt) => {
-          const cur = menuRef.current
-          const node = evt.target
-          if (cur.contains(node)) return
-          setIsOpen(false)
-        })
+        document.addEventListener(
+          `click`,
+          (evt: any) => {
+            const cur = menuRef.current
+            const node = evt.target
+            if (cur.contains(node)) return
+            setIsOpen(false)
+          },
+          false
+        )
       })
     }
     return
