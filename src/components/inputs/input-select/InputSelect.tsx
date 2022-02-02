2e77ef2c4235e6e86fac569c7792869b68473967
Rui Santiago <ruisantiagomr@gmail.com>
2022-02-02 14:47:14 +0000

    add classes and callback

--- src/components/inputs/input-select/InputSelect.tsx
+++ src/components/inputs/input-select/InputSelect.tsx
@@ -1,9 +1,25 @@
-export const MbInputSelect = () => {
+import './InputSelect.css'
+
+interface SelectProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  onValueChange: (value: string) => void
+}
+
+export const MbInputSelect = (props: SelectProps) => {
   return (
-    <select className="h-10 w-64 md:w-96 mb-4 bg-gray-100 text:black dark:bg-gray-900 dark:text-white rounded-sm mr-24 appearance-none p-8">
-      <option value="1">Option 1</option>
-      <option value="2">Option 2</option>
-      <option value="3">Option 3</option>
-    </select>
+    <div
+      className={`input-wrapper default flex items-center justify-between rounded ${
+        props.disabled ? 'disabled' : 'default '
+      }`}
+    >
+      <select
+        className="input-field"
+        disabled={props.disabled}
+        onChange={(event) => props.onValueChange(event.target.value)}
+      >
+        <option value="1">Option 1</option>
+        <option value="2">Option 2</option>
+        <option value="3">Option 3</option>
+      </select>
+    </div>
   )
 }
