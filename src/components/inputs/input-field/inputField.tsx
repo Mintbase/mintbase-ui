3e074b6445d0f0d7821104cc69ac5727596d2a2d
Rui Santiago <ruisantiagomr@gmail.com>
2022-02-10 13:26:05 +0000

    add forward ref for form react

--- src/components/inputs/input-field/inputField.tsx
+++ src/components/inputs/input-field/inputField.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { forwardRef } from 'react'
 import { getFontType } from '../../../consts/fontType'
 import { EIconName } from '../../../consts/icons'
 import { ESize } from '../../../consts/properties'
@@ -20,108 +20,114 @@ interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
   hasPercentageLabel?: boolean
 }
 
-export const MbInput = (props: InputProps) => {
-  const getIconSize = () => {
-    return inputSize === 'big' ? '24px' : '20px'
-  }
-
-  const getLabelFontType = () => {
-    switch (inputSize) {
-      case 'big':
-        return 'p-med-90'
-      case 'medium':
-        return 'p-small-90'
-      case 'small':
-        return 'cap-big-90'
+export const MbInput = forwardRef<HTMLInputElement, InputProps>(
+  (
+    {
+      id,
+      required,
+      textarea,
+      disabled,
+      className,
+      placeholder,
+      controlStatus = EControlStatus.NORMAL,
+      label,
+      onChange,
+      hasPercentageLabel,
+      value,
+      type,
+      hasIcon,
+      inputSize = ESize.MEDIUM,
+      ...restProps
+    },
+    ref
+  ) => {
+    const getIconSize = () => {
+      return inputSize === 'big' ? '24px' : '20px'
     }
-  }
 
-  const {
-    required,
-    textarea,
-    disabled,
-    className,
-    placeholder,
-    controlStatus = EControlStatus.NORMAL,
-    label,
-    onChange,
-    hasPercentageLabel,
-    value,
-    type,
-    hasIcon,
-    inputSize = ESize.MEDIUM,
-    ...restProps
-  } = props
+    const getLabelFontType = () => {
+      switch (inputSize) {
+        case 'big':
+          return 'p-med-90'
+        case 'medium':
+          return 'p-small-90'
+        case 'small':
+          return 'cap-big-90'
+      }
+    }
 
-  return (
-    <>
-      {label && (
-        <label className={`block mb-8 dark:text-white ${getLabelFontType()}`}>
-          {label}
-          {required && (
-            <span className="text-error-300 dark:text-error-100"> *</span>
-          )}
-        </label>
-      )}
-      <div
-        className={`input-wrapper ${inputSize} flex items-center justify-between rounded ${
-          disabled ? 'disabled' : 'default ' + controlStatus
-        } ${textarea ? 'textarea' : ''} ${className}`}
-      >
-        <label className="flex w-full">
-          {textarea ? (
-            <textarea
-              className={`input-field textarea ${getFontType(inputSize)}`}
-              placeholder={placeholder}
-              onChange={() => onChange}
-            ></textarea>
-          ) : (
-            <>
-              <input
-                disabled={disabled}
-                placeholder={hasPercentageLabel ? '' : placeholder}
-                type={type}
-                value={value}
-                required={required}
-                className={`input-field ${getFontType(inputSize)}`}
-                {...restProps}
-              />
-              {hasPercentageLabel && (
-                <span className={`${getFontType(inputSize)} text-gray-500`}>
-                  %
-                </span>
-              )}
-            </>
-          )}
-        </label>
-
-        {!textarea && hasIcon && (
-          <div className="flex">
-            {controlStatus === EControlStatus.VALID ? (
-              <MbIcon
-                name={EIconName.SUCCESS}
-                size={getIconSize()}
-                color="success-300"
-                darkColor="success-100"
-              />
-            ) : controlStatus === EControlStatus.INVALID ? (
-              <MbIcon
-                name={EIconName.ERROR}
-                size={getIconSize()}
-                color="error-300 "
-                darkColor="error-100"
-              />
-            ) : (
-              <MbIcon
-                name={EIconName.INFO}
-                size={getIconSize()}
-                color="blue-300"
-                darkColor="blue-100"
-              />
+    return (
+      <>
+        {label && (
+          <label className={`block mb-8 dark:text-white ${getLabelFontType()}`}>
+            {label}
+            {required && (
+              <span className="text-error-300 dark:text-error-100"> *</span>
             )}
-          </div>
+          </label>
         )}
-      </div>
-    </>
-  )
-}
+        <div
+          className={`input-wrapper ${inputSize} flex items-center justify-between rounded ${
+            disabled ? 'disabled' : 'default ' + controlStatus
+          } ${textarea ? 'textarea' : ''} ${className}`}
+        >
+          <label className="flex w-full">
+            {textarea ? (
+              <textarea
+                className={`input-field textarea ${getFontType(inputSize)}`}
+                placeholder={placeholder}
+                onChange={() => onChange}
+              ></textarea>
+            ) : (
+              <>
+                <input
+                  id={id}
+                  disabled={disabled}
+                  ref={ref}
+                  placeholder={hasPercentageLabel ? '' : placeholder}
+                  type={type}
+                  value={value}
+                  required={required}
+                  className={`input-field ${getFontType(inputSize)}`}
+                  {...restProps}
+                />
+                {hasPercentageLabel && (
+                  <span className={`${getFontType(inputSize)} text-gray-500`}>
+                    %
+                  </span>
+                )}
+              </>
+            )}
+          </label>
+
+          {!textarea && hasIcon && (
+            <div className="flex">
+              {controlStatus === EControlStatus.VALID ? (
+                <MbIcon
+                  name={EIconName.SUCCESS}
+                  size={getIconSize()}
+                  color="success-300"
+                  darkColor="success-100"
+                />
+              ) : controlStatus === EControlStatus.INVALID ? (
+                <MbIcon
+                  name={EIconName.ERROR}
+                  size={getIconSize()}
+                  color="error-300 "
+                  darkColor="error-100"
+                />
+              ) : (
+                <MbIcon
+                  name={EIconName.INFO}
+                  size={getIconSize()}
+                  color="blue-300"
+                  darkColor="blue-100"
+                />
+              )}
+            </div>
+          )}
+        </div>
+      </>
+    )
+  }
+)
