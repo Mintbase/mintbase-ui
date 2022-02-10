e344da48a1e703b65b4357ca15f3d4c94b4d425b
Rui Santiago <ruisantiagomr@gmail.com>
2022-02-10 14:36:50 +0000

    update textarea

--- src/components/inputs/textarea/TextArea.tsx
+++ src/components/inputs/textarea/TextArea.tsx
@@ -1,7 +1,8 @@
 import { forwardRef } from 'react'
-import { getFontType } from '../../../consts/fontType'
+import { getFontType, getInputLabelFontType } from '../../../consts/fontType'
 import { ESize } from '../../../consts/properties'
 import { EControlStatus } from '../../inputs/input-field/inputField'
+import './TextArea.css'
 
 interface TextareaProps
   extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {
@@ -28,13 +29,36 @@ export const MbTextArea = forwardRef<HTMLTextAreaElement, TextareaProps>(
     ref
   ) => {
     return (
-      <textarea
-        className={`input-field textarea ${getFontType(inputSize)}`}
-        placeholder={placeholder}
-        value={value}
-        {...restProps}
-        ref={ref}
-      ></textarea>
+      <>
+        {' '}
+        {label && (
+          <label
+            className={`block mb-8 dark:text-white ${getInputLabelFontType(
+              inputSize
+            )}`}
+          >
+            {label}
+            {required && (
+              <span className="text-error-300 dark:text-error-100"> *</span>
+            )}
+          </label>
+        )}{' '}
+        <div
+          className={`input-wrapper ${inputSize} flex items-center justify-between rounded ${
+            disabled ? 'disabled' : 'default ' + controlStatus
+          } ${className}`}
+        >
+          <label className="flex w-full">
+            <textarea
+              className={`textarea ${getFontType(inputSize)}`}
+              placeholder={placeholder}
+              value={value}
+              {...restProps}
+              ref={ref}
+            ></textarea>
+          </label>
+        </div>
+      </>
     )
   }
 )
