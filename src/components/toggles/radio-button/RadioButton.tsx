4bce4d1a10ed3feb869cc1f58fb4a0b60a07abaa
Rui Santiago <ruisantiagomr@gmail.com>
2022-02-10 15:22:22 +0000

    add ref to radio button

--- src/components/toggles/radio-button/RadioButton.tsx
+++ src/components/toggles/radio-button/RadioButton.tsx
@@ -1,32 +1,31 @@
+import { forwardRef } from 'react'
 import { TToggle } from '../../../types/toggles.type'
 import './radiobutton.css'
 
-interface RadioButtonProps extends TToggle {
-  handleChange: (id: string) => void
-}
+interface RadioProps
+  extends TToggle,
+    React.InputHTMLAttributes<HTMLInputElement> {}
 
-export const MbRadioButton = (props: RadioButtonProps) => {
-  const { label, id, isChecked, disabled, handleChange } = props
-
-  return (
-    <div
-      className={`flex items-center radio-item ${disabled ? 'disabled' : ''}`}
-    >
-      <input
-        type="radio"
-        className="cursor-pointer"
-        id={id}
-        checked={isChecked}
-        onChange={(e) => {
-          if (disabled) return
-          handleChange(e.target.id)
-        }}
-      />
-      <label htmlFor={id}>
-        <span className="p-med-90 pl-12 dark:text-white cursor-pointer">
-          {label}
-        </span>
-      </label>
-    </div>
-  )
-}
+export const MbRadioButton = forwardRef<HTMLInputElement, RadioProps>(
+  ({ id, label, checked, disabled, ...restProps }, ref) => {
+    return (
+      <div
+        className={`flex items-center radio-item ${disabled ? 'disabled' : ''}`}
+      >
+        <input
+          type="radio"
+          className="cursor-pointer"
+          id={id}
+          checked={checked}
+          ref={ref}
+          {...restProps}
+        />
+        <label htmlFor={id}>
+          <span className="p-med-90 pl-12 dark:text-white cursor-pointer">
+            {label}
+          </span>
+        </label>
+      </div>
+    )
+  }
+)
