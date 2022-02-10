a0105e99a3933864d3bb5e870bef54604d0a0ba5
Rui Santiago <ruisantiagomr@gmail.com>
2022-02-10 14:22:14 +0000

    add text area story

---
+++ src/stories/components/inputs/TextArea.stories.tsx
@@ -0,0 +1,40 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react'
+import React from 'react'
+
+import { ESize } from '../../../consts/properties'
+import { MbTextArea, EControlStatus } from '../../../index'
+
+export default {
+  title: 'Components/TextAreas',
+  component: MbTextArea,
+  argTypes: {
+    controlStatus: {
+      options: ['normal', 'valid', 'invalid'],
+      control: { type: 'radio' },
+    },
+    inputSize: {
+      options: ['small', 'medium', 'big'],
+      control: { type: 'radio' },
+    },
+  },
+} as ComponentMeta<typeof MbTextArea>
+
+const Template: ComponentStory<typeof MbTextArea> = (args) => (
+  <MbTextArea {...args} />
+)
+
+export const TextArea = Template.bind({})
+TextArea.args = {
+  label: 'Label',
+  hasIcon: true,
+  disabled: false,
+  placeholder: 'Placeholder',
+  type: 'text',
+  required: true,
+  textarea: false,
+  controlStatus: EControlStatus.NORMAL,
+  TextAreaSize: ESize.MEDIUM,
+  onChange: (event: any) => {
+    console.log(event.target.value)
+  },
+}
