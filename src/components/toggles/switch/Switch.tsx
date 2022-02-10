b73254f3ebd006b044c5c0287fb86f72e07959d0
Rui Santiago <ruisantiagomr@gmail.com>
2022-02-10 15:22:28 +0000

    add ref to switch

--- src/components/toggles/switch/Switch.tsx
+++ src/components/toggles/switch/Switch.tsx
@@ -2,35 +2,35 @@ import { MbIcon } from '../../icon/Icon'
 import { EIconName } from '../../../consts/icons'
 import './switch.css'
 import { TToggle } from '../../../types/toggles.type'
+import { forwardRef } from 'react'
 
-interface SwitchProps extends TToggle {
-  handleChange: (checked: boolean) => void
-}
+interface SwitchProps
+  extends TToggle,
+    React.InputHTMLAttributes<HTMLInputElement> {}
 
-export const MbSwitch = (props: SwitchProps) => {
-  const { id, isChecked, disabled, handleChange } = props
-  return (
-    <div className={`switch ${disabled ? 'disabled' : ''}`}>
-      <input
-        type="checkbox"
-        className="switch-checkbox"
-        id={id}
-        checked={isChecked}
-        onChange={(e) => {
-          if (disabled) return
-          handleChange(e.target.checked)
-        }}
-      />
-      <label className="switch-label" htmlFor={id}>
-        <span className="switch-btn">
-          <MbIcon
-            name={isChecked ? EIconName.CHECK : EIconName.CLOSE}
-            size="16px"
-            color="white"
-            darkColor="black"
-          />
-        </span>
-      </label>
-    </div>
-  )
-}
+export const MbSwitch = forwardRef<HTMLInputElement, SwitchProps>(
+  ({ id, label, checked, disabled, ...restProps }, ref) => {
+    return (
+      <div className={`switch ${disabled ? 'disabled' : ''}`}>
+        <input
+          type="checkbox"
+          className="switch-checkbox"
+          id={id}
+          checked={checked}
+          ref={ref}
+          {...restProps}
+        />
+        <label className="switch-label" htmlFor={id}>
+          <span className="switch-btn">
+            <MbIcon
+              name={checked ? EIconName.CHECK : EIconName.CLOSE}
+              size="16px"
+              color="white"
+              darkColor="black"
+            />
+          </span>
+        </label>
+      </div>
+    )
+  }
+)
