cd9280e829d349f31407a39ae1f286d13b034d71
Rui Santiago <ruisantiagomr@gmail.com>
2022-02-10 15:13:24 +0000

    add ref to checkbox

--- src/components/toggles/checkbox/Checkbox.tsx
+++ src/components/toggles/checkbox/Checkbox.tsx
@@ -2,43 +2,39 @@ import { MbIcon } from '../../icon/Icon'
 import { EIconName } from '../../../consts/icons'
 import './checkbox.css'
 import { TToggle } from '../../../types/toggles.type'
-interface CheckboxProps extends TToggle {
-  handleChange: (checked: boolean) => void
+import { forwardRef } from 'react'
+
+interface CheckboxProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  label?: string
 }
 
-export const MbCheckbox = (props: CheckboxProps) => {
-  const { label, id, isChecked, disabled, handleChange } = props
-  return (
-    <>
-      <label className="flex items-end">
-        <input
-          type="checkbox"
-          id={id}
-          checked={isChecked}
-          onChange={(e) => {
-            if (disabled) return
-            handleChange(e.target.checked)
-          }}
-        />
-        <div
-          className={`checkbox-item ${disabled ? 'disabled' : ''} ${
-            isChecked ? 'active' : ''
-          }`}
-          aria-hidden="true"
-        >
-          {isChecked && (
-            <MbIcon
-              name={EIconName.CHECK}
-              size="14px"
-              color="white"
-              darkColor="black"
-            />
-          )}
-        </div>
-        <span className="p-med-90 pl-12 dark:text-white cursor-pointer">
-          {label}
-        </span>
-      </label>
-    </>
-  )
-}
+export const MbCheckbox = forwardRef<HTMLInputElement, CheckboxProps>(
+  ({ id, label, checked, disabled, ...restProps }, ref) => {
+    return (
+      <>
+        <label className="flex items-end">
+          <input type="checkbox" id={id} checked={checked} {...restProps} />
+          <div
+            className={`checkbox-item ${disabled ? 'disabled' : ''} ${
+              checked ? 'active' : ''
+            }`}
+            aria-hidden="true"
+            ref={ref}
+          >
+            {checked && (
+              <MbIcon
+                name={EIconName.CHECK}
+                size="14px"
+                color="white"
+                darkColor="black"
+              />
+            )}
+          </div>
+          <span className="p-med-90 pl-12 dark:text-white cursor-pointer">
+            {label}
+          </span>
+        </label>
+      </>
+    )
+  }
+)
