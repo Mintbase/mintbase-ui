677b1dfe59920d1961159f3a5353053519d706a2
Rui Santiago <ruisantiagomr@gmail.com>
2022-02-10 14:36:40 +0000

    update font type

--- src/components/inputs/input-field/inputField.tsx
+++ src/components/inputs/input-field/inputField.tsx
@@ -1,5 +1,5 @@
 import React, { forwardRef } from 'react'
-import { getFontType } from '../../../consts/fontType'
+import { getFontType, getInputLabelFontType } from '../../../consts/fontType'
 import { EIconName } from '../../../consts/icons'
 import { ESize } from '../../../consts/properties'
 import { MbIcon } from '../../icon/Icon'
@@ -42,21 +42,10 @@ export const MbInput = forwardRef<HTMLInputElement, InputProps>(
       return inputSize === 'big' ? '24px' : '20px'
     }
 
-    const getLabelFontType = () => {
-      switch (inputSize) {
-        case 'big':
-          return 'p-med-90'
-        case 'medium':
-          return 'p-small-90'
-        case 'small':
-          return 'cap-big-90'
-      }
-    }
-
     return (
       <>
         {label && (
-          <label className={`block mb-8 dark:text-white ${getLabelFontType()}`}>
+          <label className={`block mb-8 dark:text-white ${getInputLabelFontType(inputSize)}`}>
             {label}
             {required && (
               <span className="text-error-300 dark:text-error-100"> *</span>
