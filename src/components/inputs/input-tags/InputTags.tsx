6fca02f08433416d51ead78e7f15a7e280e25756
Rui Santiago <ruisantiagomr@gmail.com>
2022-03-02 13:03:25 +0000

    add ref

--- src/components/inputs/input-tags/InputTags.tsx
+++ src/components/inputs/input-tags/InputTags.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { forwardRef, useEffect, useState } from 'react'
 import { MbIcon } from '../..'
 import { EIconName, ESize, getFontType } from '../../..'
 import './../Input.css'
@@ -21,91 +21,110 @@ interface InputTagsProps extends React.InputHTMLAttributes<HTMLInputElement> {
   onMaxTags: () => void
 }
 
-export const MbInputTags = (props: InputTagsProps) => {
-  const { label, placeholder, maxTags, onTagsChange } = props
-  const [localTags, setLocalTags] = useState<string[]>([])
-  const [isInvalid, setIsInvalid] = useState(false)
+export const MbInputTags = forwardRef<HTMLInputElement, InputTagsProps>(
+  (
+    {
+      id,
+      required,
+      disabled,
+      className,
+      placeholder,
+      label,
+      maxTags,
+      value,
+      type,
+      onTagsChange,
+      onMaxTags,
+      ...restProps
+    },
+    ref
+  ) => {
+    const [localTags, setLocalTags] = useState<string[]>([])
+    const [isInvalid, setIsInvalid] = useState(false)
 
-  const handleKeyUp = (event: any) => {
-    const keyCode = event.keyCode ? event.keyCode : event.which
+    const handleKeyUp = (event: any) => {
+      const keyCode = event.keyCode ? event.keyCode : event.which
 
-    if (isInvalid) return
+      if (isInvalid) return
 
-    if (keyCode === ENTER_KEY_CODE || keyCode === COMMA_KEY_CODE) {
-      const tag = event.target.value.trim().split(',')
+      if (keyCode === ENTER_KEY_CODE || keyCode === COMMA_KEY_CODE) {
+        const tag = event.target.value.trim().split(',')
 
-      if (tag.length === 0 || tag[0] === '') {
+        if (tag.length === 0 || tag[0] === '') {
+          return
+        }
+
+        setLocalTags([...localTags, tag[0]])
+
+        event.target.value = ''
+      }
+    }
+
+    const handleKeyDown = (event: any) => {
+      preventEnterSubmit(event)
+      if (isInvalid) return
+
+      const keyCode = event.keyCode ? event.keyCode : event.which
+
+      if (keyCode === DELETE_KEY_CODE && event.target.value === '') {
+        removeTag(localTags.length - 1)
         return
       }
-
-      setLocalTags([...localTags, tag[0]])
-
-      event.target.value = ''
     }
-  }
 
-  const handleKeyDown = (event: any) => {
-    preventEnterSubmit(event)
-    if (isInvalid) return
+    const removeTag = (index: number) => {
+      const _tags = localTags.slice()
 
-    const keyCode = event.keyCode ? event.keyCode : event.which
-
-    if (keyCode === DELETE_KEY_CODE && event.target.value === '') {
-      removeTag(localTags.length - 1)
-      return
+      _tags.splice(index, 1)
+      setLocalTags(_tags)
     }
-  }
 
-  const removeTag = (index: number) => {
-    const _tags = localTags.slice()
+    useEffect(() => {
+      setIsInvalid(localTags.length > maxTags)
+      onTagsChange(localTags)
+    }, [localTags])
 
-    _tags.splice(index, 1)
-    setLocalTags(_tags)
-  }
+    return (
+      <>
+        <label className="label">{label}</label>
+        <div
+          className={`main-input default ${
+            isInvalid ? 'invalid' : ''
+          } input-tags no-scrollbar`}
+        >
+          {localTags?.map((tag, index) => (
+            <div key={index} className="mr-8">
+              <div className="flex gap-0.5 items-center rounded-full w-max cursor-pointer relative bg-blue-300 dark:bg-blue-100 py-4 px-8">
+                <div onClick={() => removeTag(index)}>
+                  <MbIcon
+                    name={EIconName.CLOSE}
+                    size="16px"
+                    color="white"
+                    darkColor="black"
+                  />
+                </div>
 
-  useEffect(() => {
-    setIsInvalid(localTags.length > maxTags)
-    onTagsChange(localTags)
-  }, [localTags])
-
-  return (
-    <>
-      <label className="label">{label}</label>
-      <div
-        className={`main-input default ${
-          isInvalid ? 'invalid' : ''
-        } input-tags no-scrollbar`}
-      >
-        {localTags?.map((tag, index) => (
-          <div key={index} className="mr-8">
-            <div className="flex gap-0.5 items-center rounded-full w-max cursor-pointer relative bg-blue-300 dark:bg-blue-100 py-4 px-8">
-              <div onClick={() => removeTag(index)}>
-                <MbIcon
-                  name={EIconName.CLOSE}
-                  size="16px"
-                  color="white"
-                  darkColor="black"
-                />
-              </div>
-
-              <div className="p-med-90 pt-2 text-white dark:text-black">
-                {tag}
+                <div className="p-med-90 pt-2 text-white dark:text-black">
+                  {tag}
+                </div>
               </div>
             </div>
-          </div>
-        ))}
-        <label className="flex w-full">
-          <input
-            id="tags"
-            ref={'tags'}
-            placeholder={localTags.length > 0 ? '' : placeholder}
-            type="text"
-            className={`input-field-tags ${getFontType(ESize.BIG)}`}
-            onKeyDown={handleKeyDown}
-            onKeyUp={handleKeyUp}
-          />
-        </label>
-      </div>
-    </>
-  )
-}
+          ))}
+          <label className="flex w-full">
+            <input
+              id={id}
+              ref={ref}
+              placeholder={localTags.length > 0 ? '' : placeholder}
+              type="text"
+              value={value}
+              className={`input-field-tags ${getFontType(ESize.BIG)}`}
+              onKeyDown={handleKeyDown}
+              onKeyUp={handleKeyUp}
+              {...restProps}
+            />
+          </label>
+        </div>
+      </>
+    )
+  }
+)
