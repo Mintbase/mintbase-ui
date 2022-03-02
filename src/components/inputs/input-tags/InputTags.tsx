37d53692db64367804139b52d789e7d812d28f93
Rui Santiago <ruisantiagomr@gmail.com>
2022-03-02 09:17:59 +0000

    wip add css classes

--- src/components/inputs/input-tags/InputTags.tsx
+++ src/components/inputs/input-tags/InputTags.tsx
@@ -1,5 +1,7 @@
 import { MbIcon } from '../..'
 import { EIconName } from '../../..'
+import './../Input.css'
+import './InputTags.css'
 
 interface InputTagsProps {
   label: string
@@ -16,17 +18,17 @@ export const MbInputTags = (props: InputTagsProps) => {
   return (
     <>
       <label className="label">{ label }</label>
-      <div className="wrap-options">
+      <div className="main-input default input-tags">
         {tags?.map((tag, index) => (
           <div key={index}>
-            <div className="flex gap-4 items-center rounded-full w-max cursor-pointer relative bg-blue-300 dark:bg-blue-100">
+            <div className="flex gap-0.5 items-center rounded-full w-max cursor-pointer relative bg-blue-300 dark:bg-blue-100 py-4 px-8">
               <MbIcon
                 name={EIconName.CLOSE}
                 size="16px"
                 color="white"
                 darkColor="black"
               />
-              <div className="p-big-90 pt-2 text-white dark:text-black">
+              <div className="p-med-90 pt-2 text-white dark:text-black">
                 {tag.label}
               </div>
             </div>
