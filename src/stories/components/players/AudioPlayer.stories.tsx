50a45b8f6f8de74b2c00a9f921a82d9bb4e58263
Rui Santiago <ruisantiagomr@gmail.com>
2022-03-07 18:01:29 +0000

    add storybook

---
+++ src/stories/components/players/AudioPlayer.stories.tsx
@@ -0,0 +1,16 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react'
+import { MbAudioPlayer } from '../../../components'
+export default {
+  title: 'Components/Players',
+  component: MbAudioPlayer,
+  argTypes: {},
+} as ComponentMeta<typeof MbAudioPlayer>
+
+const Template: ComponentStory<typeof MbAudioPlayer> = (args) => (
+  <MbAudioPlayer {...args}></MbAudioPlayer>
+)
+
+export const Accordion = Template.bind({})
+Accordion.args = {
+  src: 'https://arweave.net/iRgcsFwp3l8nG3hld4WEQioP384c5QuFpXxvqqFJloQ',
+}
