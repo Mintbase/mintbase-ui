b499905b4a12efed2e209ae9d9b50f12425faf45
Rui Santiago <ruisantiagomr@gmail.com>
2022-03-08 13:46:20 +0000

    wip

--- src/components/players/video/VideoPlayer.tsx
+++ src/components/players/video/VideoPlayer.tsx
@@ -1,9 +1,7 @@
-const MbVideoPlayer = ({ src }: { src: string }) => {
+export const MbVideoPlayer = ({ src }: { src: string }) => {
   return (
     <video width="750" height="500" playsInline autoPlay loop>
       <source src={src} type="video/mp4" />
     </video>
   )
 }
-
-export default MbVideoPlayer
