fffc54e68296ad9e2e974f0433b5fc07e20b0560
Rui Santiago <ruisantiagomr@gmail.com>
2022-03-08 13:41:58 +0000

    add default video player

---
+++ src/components/players/video/VideoPlayer.tsx
@@ -0,0 +1,9 @@
+const MbVideoPlayer = () => {
+  return (
+    <video width="750" height="500" controls>
+      <source src="..Videos/video1.mp4" type="video/mp4" />
+    </video>
+  )
+}
+
+export default MbVideoPlayer
