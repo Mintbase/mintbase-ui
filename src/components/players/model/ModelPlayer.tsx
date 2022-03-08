1b5f3f31f9a68e459e446620df6b88c398b6dd30
Rui Santiago <ruisantiagomr@gmail.com>
2022-03-08 16:28:29 +0000

    add logic to modelplayer

--- src/components/players/model/ModelPlayer.tsx
+++ src/components/players/model/ModelPlayer.tsx
@@ -1,25 +1,46 @@
-import { useEffect } from 'react'
+import { useEffect, useRef, useState } from 'react'
+import { MbAction } from '../../action/Action'
 
-export const MbModelPlayer = ({modelSrc, imgSrc }:{modelSrc: string, imgSrc: string}) => {
+export const MbModelPlayer = ({
+  modelSrc,
+  posterImg,
+  ...restProps
+}: {
+  modelSrc: string
+  posterImg: JSX.Element
+}) => {
+  const [animations, setAnimations] = useState<any>([])
   useEffect(() => {
     import('@google/model-viewer/dist/model-viewer')
   }, [])
+
+  const elementRef = useRef<any>()
+
+  useEffect(() => {
+    elementRef.current.addEventListener('load', handleEvent)
+  }, [elementRef])
+
+  const handleEvent = () => {
+    const _animations = elementRef.current.availableAnimations ?? ['']
+
+    setAnimations([..._animations])
+  }
   return (
     <model-viewer
       style={{ height: '100%', width: '100%' }}
       ref={elementRef}
-      src={src}
-      reveal="interaction"
+      src={modelSrc}
+      //   reveal="interaction"
       animation-name={animations?.length > 0 ? animations[0] : ''}
       shadow-intensity="1"
-      autoplay={!isBasic}
-      ar={true}
+      //   autoplay={false}
+      //   ar={true}
       ar-modes="webxr scene-viewer quick-look"
       camera-controls
       {...restProps}
     >
-      <Poster src={poster} slot="poster" />
-      <ShowModelButton slot="poster">Click to show 3D model</ShowModelButton>
+      <div slot="poster">{posterImg}</div>
+      <MbAction slot="poster">Click to show 3D model</MbAction>
     </model-viewer>
   )
 }
