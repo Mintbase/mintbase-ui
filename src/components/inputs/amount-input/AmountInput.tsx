ccfc0a9054a9dc43b6556853315bc6dbc2d72e07
Rui Santiago <ruisantiagomr@gmail.com>
2022-03-10 14:42:41 +0000

    fix: input amount forward ref

--- src/components/inputs/amount-input/AmountInput.tsx
+++ src/components/inputs/amount-input/AmountInput.tsx
@@ -1,97 +1,115 @@
-import { useEffect, useState } from 'react'
+import { forwardRef, useEffect, useState } from 'react'
 import { ESize, getFontType } from '../../..'
 import './AmountInput.css'
 
 interface AmountButtonProps
   extends React.InputHTMLAttributes<HTMLInputElement> {
   maxAmount: number
-  disabled?: boolean
   btnSize?: ESize
   onValueChange: (amount: number) => void
 }
 
-export const MbAmountInput = (props: AmountButtonProps) => {
-  const [amount, setAmount] = useState<any>(1)
+export const MbAmountInput = forwardRef<HTMLInputElement, AmountButtonProps>(
+  (
+    {
+      id,
+      required,
+      maxAmount = 50,
+      disabled,
+      btnSize = ESize.MEDIUM,
+      placeholder,
+      value,
+      type,
+      onValueChange,
+      ...restProps
+    },
+    ref
+  ) => {
+    const [amount, setAmount] = useState<any>(1)
 
-  const {
-    maxAmount = 50,
-    disabled,
-    btnSize = ESize.MEDIUM,
-    onValueChange,
-    ...restProps
-  } = props
-
-  const handlePlus = () => {
-    if (disabled) return
-    const updated = amount + 1
-    if (updated === maxAmount + 1) return
-    setAmount(amount + 1)
-  }
-
-  const handleMinus = () => {
-    if (disabled) return
-    if (amount === 0) return
-    const updated = amount - 1
-    if (updated <= 0) return
-    setAmount(amount - 1)
-  }
-
-  const handleContentChanges = (event: React.ChangeEvent<HTMLInputElement>) => {
-    event.preventDefault()
-    const value = event.target.value
-    const isNumber = RegExp(/^\d*?$/).test(value)
-
-    if (value === '' || value === '0' || !isNumber) {
-      setAmount(undefined)
-      return
+    const handlePlus = () => {
+      if (disabled) return
+      const updated = amount + 1
+      if (updated === maxAmount + 1) return
+      setAmount(amount + 1)
     }
 
-    console.log(Number(value), maxAmount)
-    if (Number(value) <= maxAmount) {
-      setAmount(Number(value))
+    const handleMinus = () => {
+      if (disabled) return
+      if (amount === 0) return
+      const updated = amount - 1
+      if (updated <= 0) return
+      setAmount(amount - 1)
     }
-  }
 
-  useEffect(() => {
-    onValueChange(amount)
-  }, [amount])
+    const handleContentChanges = (
+      event: React.ChangeEvent<HTMLInputElement>
+    ) => {
+      event.preventDefault()
+      const value = event.target.value
+      const isNumber = RegExp(/^\d*?$/).test(value)
 
-  return (
-    <div className="flex">
-      <div
-        className={` ${getFontType(
-          btnSize
-        )} flex bg-gray-100 dark:bg-gray-900 p-8 rounded w-full ${
-          disabled ? 'bg-gray-200 dark:bg-gray-700' : ''
-        }`}
-      >
-        <input
-          className={`${
-            btnSize === ESize.SMALL ? 'my-2' : 'ml-4 mr-12'
-          } w-full dark:text-white outline-none bg-transparent mt-2 ${
-            disabled ? 'text-gray-500' : ''
+      if (value === '' || value === '0' || !isNumber) {
+        setAmount(undefined)
+        return
+      }
+
+      console.log(Number(value), maxAmount)
+      if (Number(value) <= maxAmount) {
+        setAmount(Number(value))
+      }
+    }
+
+    useEffect(() => {
+      onValueChange(amount)
+    }, [amount])
+
+    return (
+      <div className="flex">
+        <div
+          className={` ${getFontType(
+            btnSize
+          )} flex bg-gray-100 dark:bg-gray-900 p-8 rounded w-full ${
+            disabled ? 'bg-gray-200 dark:bg-gray-700' : ''
           }`}
-          placeholder={'1'}
-          onChange={handleContentChanges}
-          type="number"
-          value={amount}
-          {...restProps}
-        />
-        <div className={`flex ${btnSize === ESize.SMALL ? 'gap-8' : 'gap-12'}`}>
-          <button
-            className={`amount-button ${disabled ? 'disabled' : ''} ${btnSize}`}
-            onClick={handleMinus}
+        >
+          <input
+            className={`${
+              btnSize === ESize.SMALL ? 'my-2' : 'ml-4 mr-12'
+            } w-full dark:text-white outline-none bg-transparent mt-2 ${
+              disabled ? 'text-gray-500' : ''
+            }`}
+            ref={ref}
+            placeholder={'1'}
+            disabled={disabled}
+            required={required}
+            onChange={handleContentChanges}
+            type="number"
+            value={amount}
+            {...restProps}
+          />
+          <div
+            className={`flex ${btnSize === ESize.SMALL ? 'gap-8' : 'gap-12'}`}
           >
-            <span className="mt-2">-</span>
-          </button>
-          <button
-            className={`amount-button ${disabled ? 'disabled' : ''} ${btnSize}`}
-            onClick={handlePlus}
-          >
-            <span className="mt-2">+</span>
-          </button>
+            <button
+              className={`amount-button ${
+                disabled ? 'disabled' : ''
+              } ${btnSize}`}
+              onClick={handleMinus}
+            >
+              <span className="mt-2">-</span>
+            </button>
+            <button
+              className={`amount-button ${
+                disabled ? 'disabled' : ''
+              } ${btnSize}`}
+              onClick={handlePlus}
+            >
+              <span className="mt-2">+</span>
+            </button>
+          </div>
         </div>
       </div>
-    </div>
-  )
-}
+    )
+  }
+)
