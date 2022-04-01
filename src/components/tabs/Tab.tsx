ea67dda4e2c3163fc1e308c9c68cc0b2de27222c
Rui Santiago <ruisantiagomr@gmail.com>
2022-04-01 19:08:01 +0100

    :bug: add small prop in tab component

--- src/components/tabs/Tab.tsx
+++ src/components/tabs/Tab.tsx
@@ -4,16 +4,21 @@ import './tab.css'
 export interface TabProps extends React.HTMLAttributes<HTMLDivElement> {
   label: JSX.Element
   isActive?: boolean
+  isSmall?: boolean
   extraFilter?: string
   onExtraFilterChange?: (isSelected: boolean) => void
 }
 
 export const MbTab = (props: TabProps) => {
-  const { label, isActive } = props
+  const { label, isActive, isSmall } = props
   return (
     <>
       <div className="flex">
-        <div className={`tab p-med-90 ${isActive ? 'active' : ''}`}>
+        <div
+          className={`tab p-med-90 ${isActive ? 'active' : ''} ${
+            isSmall ? 'small' : ''
+          }`}
+        >
           {label}
         </div>
       </div>
