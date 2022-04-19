2a268dd5770dfa3f93ef90169844ff6ea704a7df
cif <email@benipsen.com>
2022-04-19 13:55:07 -0400

    🎨 Add validation of text types to HTML tag

--- src/components/text/Text.tsx
+++ src/components/text/Text.tsx
@@ -1,8 +1,10 @@
 import React, { createElement } from 'react'
 import './text.css'
 
+type SemanticHtmlTextTypes = 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 'p' | 'span' | 'em' 
+
 type TextProps = { 
-  semanticHtmlTag?: string,
+  semanticHtmlTag?: SemanticHtmlTextTypes,
 } & React.HTMLAttributes<HTMLDivElement>
 
 export const MbText = (props: TextProps) => {
