24af405bb7298376c34efd1ee499b29e2e4fd8d0
cif <email@benipsen.com>
2022-04-19 13:51:19 -0400

    ✨ Add semantic tag option for MbText

--- src/components/text/Text.tsx
+++ src/components/text/Text.tsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import React, { createElement } from 'react'
 import './text.css'
 
-type TextProps = React.HTMLAttributes<HTMLDivElement>
+type TextProps = { 
+  semanticHtmlTag?: string,
+} & React.HTMLAttributes<HTMLDivElement>
 
 export const MbText = (props: TextProps) => {
+  
+  if (props.semanticHtmlTag) {
+    return createElement(props.semanticHtmlTag, { className: props.className }, props.children)
+  }
   return <div className={`${props.className}`}>{props.children}</div>
 }
