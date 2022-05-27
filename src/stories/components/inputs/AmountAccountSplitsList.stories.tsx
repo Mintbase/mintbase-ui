4277aa144ad205a93d66981c23d1b992f51aaac7
Rui Santiago <ruisantiagomr@gmail.com>
2022-05-27 12:57:36 +0100

    :sparkles: Near Amount Account Input (#268)
    * :sparkles: Add Amount Account Input Component  & Splits/Transfer Component
    
    * :art: update imports and names
    
    * :lipstick: Change stories name & add button
    
    * :lipstick: Add isPercentage
    
    * :fire: remove clog
    
    * remove default percentage

---
+++ src/stories/components/inputs/AmountAccountSplitsList.stories.tsx
@@ -0,0 +1,24 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react'
+import React from 'react'
+import { MbNearAmountAccount } from '../../../components'
+
+export default {
+  title: 'Components/Inputs',
+  component: MbNearAmountAccount,
+  argTypes: {},
+} as ComponentMeta<typeof MbNearAmountAccount>
+
+const Template: ComponentStory<typeof MbNearAmountAccount> = (args) => (
+  <MbNearAmountAccount {...args} />
+)
+
+export const AmountAccountSplitsList = Template.bind({})
+AmountAccountSplitsList.args = {
+  subtitle:
+    'Split revenue clears after each sale. Needs at least two accounts. The minter will receive 100% of split revenue unless splits are added.',
+  smallSubtitle: 'Select up to 25 accounts',
+  isPercentage: true,
+  accountExists: () => {
+    return true
+  },
+}
