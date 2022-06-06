1eb5b44c421e9f193be8e5c1d56a951fb6479ece
Rui Santiago <ruisantiagomr@gmail.com>
2022-06-06 15:30:43 +0100

    ✨  Row Select List Component (#299)
    * :construction: Row Select List Component
    
    * :sparkles: Row Select List Component

---
+++ src/components/row-select/RowSelectList.tsx
@@ -0,0 +1,144 @@
+import { useState } from 'react'
+import { EIconName, ESize, EState, EType } from '../../consts'
+import { MbAction } from '../action/Action'
+import { MbButton } from '../buttons/button/Button'
+import { MbInfoCard } from '../cards/info/InfoCard'
+import { MbIcon } from '../icon/Icon'
+import { MbInputAccount } from '../inputs/input-account/InputAccount'
+import { MbBody, MbHead, MbTable } from '../table/Table'
+import { MbText } from '../text/Text'
+import { MbCheckbox } from '../toggles/checkbox/Checkbox'
+
+export const MbRowSelectList = ({
+  elements,
+  deleteRow,
+  addMinters,
+  removeMinters,
+}: {
+  elements: { id: string; content: JSX.Element; isDisabled: boolean }[]
+  deleteRow: (id: string) => void
+  addMinters: () => void
+  removeMinters: (ids: string[]) => void
+}) => {
+  const [checkedList, setCheckedList] = useState<string[]>([])
+
+  const isElementSelected = (id: string) => {
+    return checkedList.includes(id)
+  }
+
+  return (
+    <MbInputAccount
+      smallSubtitle=""
+      subtitle=""
+      inputs={
+        <MbTable>
+          <MbHead>
+            <tr>
+              <td>Account</td>
+            </tr>
+          </MbHead>
+          <MbBody>
+            {elements &&
+              elements.map((element) => {
+                const { id, content, isDisabled } = element
+                console.log(id)
+                return (
+                  <tr
+                    key={id}
+                    className={`${
+                      isElementSelected(id)
+                        ? 'bg-blue-300-15 dark:bg-blue-100-15'
+                        : ''
+                    }`}
+                  >
+                    <td className="flex items-center gap-12">
+                      {!isDisabled && (
+                        <MbCheckbox
+                          checked={isElementSelected(id)}
+                          onChange={(e) => {
+                            if (isElementSelected(id)) {
+                              setCheckedList(
+                                checkedList.filter((elm) => elm !== id)
+                              )
+                              return
+                            }
+                            setCheckedList([...checkedList, id])
+                          }}
+                        />
+                      )}
+
+                      {content}
+                    </td>
+                    {isDisabled ? (
+                      <></>
+                    ) : (
+                      checkedList.length < 1 && (
+                        <td>
+                          <div className="flex justify-end w-auto">
+                            <span
+                              className="cursor-pointer"
+                              onClick={() => deleteRow(id)}
+                            >
+                              <MbIcon
+                                name={EIconName.DELETE}
+                                size="24px"
+                                color="blue-300"
+                                darkColor="blue-100"
+                              />
+                            </span>
+                          </div>
+                        </td>
+                      )
+                    )}
+                  </tr>
+                )
+              })}
+          </MbBody>
+        </MbTable>
+      }
+      footer={
+        <div className="grid grid-cols-1 md:grid-cols-2 justify-between items-center w-full gap-24">
+          <div className="grid grid-cols-2 gap-12">
+            <MbInfoCard
+              boxInfo={{
+                description: `${elements.length}`,
+                title: 'Accounts',
+              }}
+            />
+            {checkedList.length > 0 && (
+              <MbInfoCard
+                boxInfo={{
+                  description: `${checkedList.length}`,
+                  title: 'Selected Accounts',
+                }}
+              />
+            )}
+          </div>
+          <div className="flex items-center justify-center md:justify-end gap-24">
+            {checkedList.length > 0 && (
+              <MbAction onClick={() => setCheckedList([])}>
+                <span>Clear Selection</span>
+              </MbAction>
+            )}
+
+            {checkedList.length > 0 ? (
+              <MbButton
+                btnType={EType.PRIMARY}
+                state={EState.CAUTION}
+                label="Remove"
+                onClick={() => removeMinters(checkedList)}
+              />
+            ) : (
+              <MbButton
+                btnType={EType.PRIMARY}
+                state={EState.ACTIVE}
+                label="Add Minters"
+                onClick={() => addMinters()}
+              />
+            )}
+          </div>
+        </div>
+      }
+    />
+  )
+}
---
+++ src/stories/components/RowSelectList.stories.tsx
@@ -0,0 +1,87 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react'
+import { MbRowSelectList } from '../../components/row-select/RowSelectList'
+import { MbText } from '../../components/text/Text'
+
+export default {
+  title: 'Components',
+  component: MbRowSelectList,
+  argTypes: {},
+} as ComponentMeta<typeof MbRowSelectList>
+
+export const RowSelectList: ComponentStory<typeof MbRowSelectList> = () => (
+  <MbRowSelectList
+    elements={[
+      {
+        content: (
+          <div className="cursor-pointer flex items-center gap-12">
+            <div className="inline object-fit w-6 h-6 rounded-full overflow-hidden">
+              <img
+                className="h-full w-full object-cover"
+                src="https://coldcdn.com/api/cdn/bronil/JXl58b_p9iYzeFutFC5GcDCjsxppyFt5rRkQt4Su4LU"
+              />
+            </div>
+            <MbText className="p-med-90 text-blue-300 dark:text-blue-100">
+              mintbase1.near
+            </MbText>
+          </div>
+        ),
+        id: 'mintbase1.near',
+        isDisabled: true,
+      },
+      {
+        content: (
+          <div className="cursor-pointer flex items-center gap-12">
+            <div className="inline object-fit w-6 h-6 rounded-full overflow-hidden">
+              <img
+                className="h-full w-full object-cover"
+                src="https://coldcdn.com/api/cdn/bronil/JXl58b_p9iYzeFutFC5GcDCjsxppyFt5rRkQt4Su4LU"
+              />
+            </div>
+            <MbText className="p-med-90 text-blue-300 dark:text-blue-100">
+              mintbase2.near
+            </MbText>
+          </div>
+        ),
+        id: 'mintbase2.near',
+        isDisabled: false,
+      },
+      {
+        content: (
+          <div className="cursor-pointer flex items-center gap-12">
+            <div className="inline object-fit w-6 h-6 rounded-full overflow-hidden">
+              <img
+                className="h-full w-full object-cover"
+                src="https://coldcdn.com/api/cdn/bronil/JXl58b_p9iYzeFutFC5GcDCjsxppyFt5rRkQt4Su4LU"
+              />
+            </div>
+            <MbText className="p-med-90 text-blue-300 dark:text-blue-100">
+              mintbase3.near
+            </MbText>
+          </div>
+        ),
+        id: 'mintbase3.near',
+        isDisabled: false,
+      },
+      {
+        content: (
+          <div className="cursor-pointer flex items-center gap-12">
+            <div className="inline object-fit w-6 h-6 rounded-full overflow-hidden">
+              <img
+                className="h-full w-full object-cover"
+                src="https://coldcdn.com/api/cdn/bronil/JXl58b_p9iYzeFutFC5GcDCjsxppyFt5rRkQt4Su4LU"
+              />
+            </div>
+            <MbText className="p-med-90 text-blue-300 dark:text-blue-100">
+              mintbase4.near
+            </MbText>
+          </div>
+        ),
+        id: 'mintbase4.near',
+        isDisabled: false,
+      },
+    ]}
+    deleteRow={() => null}
+    addMinters={() => null}
+    removeMinters={(ids) => console.log('remove', ids)}
+  />
+)
