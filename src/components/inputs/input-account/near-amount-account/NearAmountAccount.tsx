b149b5e15bc24bfe274752b419eb72ec4102e68a
Rui Santiago <ruisantiagomr@gmail.com>
2022-06-20 14:38:54 +0100

    :art: Add Type Tel for Mobile Behaviour in Amount Inputs

--- src/components/inputs/input-account/near-amount-account/NearAmountAccount.tsx
+++ src/components/inputs/input-account/near-amount-account/NearAmountAccount.tsx
@@ -306,7 +306,7 @@ export const MbNearAmountAccount = ({
                       <div className="w-24">
                         <MbInput
                           id={`amount-${id}`}
-                          type="number"
+                          type="tel"
                           placeholder={'1'}
                           inputSize={ESize.BIG}
                           controlStatus={EControlStatus.NORMAL}
