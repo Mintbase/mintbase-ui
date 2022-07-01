b304f21d2a0f5092cb20b1ffbf39c58341d9338c
Rui Santiago <ruisantiagomr@gmail.com>
2022-07-01 09:27:59 +0100

    :construction: Add Pill Component

---
+++ src/components/pills/Pill.tsx
@@ -0,0 +1,7 @@
+export const MbPill = ({ text }: { text: string }) => {
+  return (
+    <div className="p-6 rounded">
+      <span className="p-small-90 uppercase">{text}</span>
+    </div>
+  )
+}
