cab658bc33775e338468b30b931aad2ce910d68b
Rui Santiago <ruisantiagomr@gmail.com>
2022-08-04 15:15:44 +0100

    :fire: rm storybook changes

--- src/stories/components/Accordion.stories.tsx
+++ src/stories/components/Accordion.stories.tsx
@@ -1,5 +1,5 @@
 import { ComponentMeta, ComponentStory } from '@storybook/react'
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import { EIconName, MbIcon } from '../..'
 import { MbAccordion } from '../../components/accordion/Accordion'
 
@@ -9,35 +9,33 @@ export default {
   argTypes: {},
 } as ComponentMeta<typeof MbAccordion>
 
-const Template: ComponentStory<typeof MbAccordion> = (args) => {
-  const [img, setImg] = useState(
-    <img
-      height={200}
-      width={200}
-      src="https://coldcdn.com/api/cdn/bronil/JXl58b_p9iYzeFutFC5GcDCjsxppyFt5rRkQt4Su4LU"
-    />
-  )
-
-  useEffect(() => {
-    setTimeout(
-      () =>
-        setImg(
-          <img
-            height={400}
-            width={400}
-            src="https://coldcdn.com/api/cdn/bronil/JXl58b_p9iYzeFutFC5GcDCjsxppyFt5rRkQt4Su4LU"
-          />
-        ),
-      2000
-    )
-  })
-
-  return (
-    <MbAccordion {...args}>
-      <p className="p-24">{img}</p>
-    </MbAccordion>
-  )
-}
+const Template: ComponentStory<typeof MbAccordion> = (args) => (
+  <MbAccordion {...args}>
+    <p className="p-24">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed congue ligula
+      turpis, a egestas urna accumsan in. Aenean aliquam iaculis tortor sit amet
+      sollicitudin. Mauris sit amet tristique leo, ut semper leo. Duis consequat
+      scelerisque nisl vel pharetra. Nam nec mollis est. Proin accumsan aliquet
+      leo vitae gravida. Vestibulum molestie, ante sit amet accumsan
+      condimentum, ante leo egestas massa, quis egestas risus turpis ut est.
+      Donec vel mauris scelerisque, tincidunt risus quis, vehicula massa. Nullam
+      vitae sodales massa, in consectetur nisi. Nam lorem magna, accumsan at
+      dignissim eu, imperdiet id nisl. Aenean tempor semper dolor eget dictum.
+      Nulla velit lorem, luctus at feugiat imperdiet, ultrices pharetra ipsum.
+      Maecenas ullamcorper pulvinar magna ut posuere. Cras aliquet interdum
+      varius. Nam imperdiet nibh eu nisl dapibus, quis suscipit nibh mattis.
+      Aenean et metus et ligula fermentum commodo eget tristique nisi.
+      Suspendisse potenti. Vestibulum placerat, odio nec fringilla pretium,
+      mauris felis sodales magna, rhoncus pretium odio enim non risus. Mauris
+      dui tortor, sollicitudin vel dolor id, mattis volutpat sapien.
+      Pellentesque sit amet lobortis quam. Donec aliquam ante eget elit
+      ullamcorper sagittis eget ut tellus. Morbi mi risus, condimentum eu
+      pharetra at, porttitor hendrerit felis. Sed rutrum, felis sit amet
+      tristique eleifend, odio leo elementum mi, sed lobortis ante felis
+      ultricies turpis.
+    </p>
+  </MbAccordion>
+)
 
 export const Accordion = Template.bind({})
 Accordion.args = {
@@ -52,5 +50,4 @@ Accordion.args = {
   ),
   isFixedAccordion: false,
   isVerified: false,
-  isOpen: true,
 }
