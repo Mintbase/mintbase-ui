31bc297c291fb1aa3c1b424a6297ece3d699d34d
Ruben Marcus <ruben@rubenmarcus.dev>
2023-02-01 18:21:27 +0000

    fix tooltip

--- src/components/tooltip/Tooltip.tsx
+++ src/components/tooltip/Tooltip.tsx
@@ -3,17 +3,25 @@ import './tooltip.css'
 export const MbTooltip = ({
   text,
   component,
+  pos,
+  classname= '',
 }: {
   text: string
-  component: JSX.Element
+  component: JSX.Element,
+  pos: string
+  classname?: string
 }) => {
+
+  let cssClass = `mb-tooltip`
+  
+  if(pos === 'absolute') cssClass = `mb-tooltip tooltip-absolute`
+  
+  if(pos === 'fixed') cssClass = `mb-tooltip tooltip-fixed`
+
   return (
     <div className="relative">
-      <span className="w-min mb-tooltip flex cursor-pointer">
+      <span className={`w-min mb-tooltip flex ${cssClass} ${classname} cursor-pointer`} aria-label={text}>
         {component}
-        <span className="tooltip-text">
-        {text}
-      </span>
       </span>
     </div>
   )
