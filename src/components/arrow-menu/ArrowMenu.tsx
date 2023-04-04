bdf33c20614eea7a0c76d172a7c28e497cdbf9eb
ruisantiago <ruisantiagomr@gmail.com>
2023-04-04 11:28:39 +0100

    fix menu classes

--- src/components/arrow-menu/ArrowMenu.tsx
+++ src/components/arrow-menu/ArrowMenu.tsx
@@ -1,6 +1,6 @@
 import { EIconName } from '../../consts'
 import { MbIcon } from '../icon/Icon'
-import '../tabs/tab.css'
+import './arrowMenu.css'
 
 interface ArrowMenuProps {
   title: string
@@ -11,15 +11,14 @@ export const MbArrowMenu = (props: ArrowMenuProps) => {
   const { title, isActive } = props
   return (
     <div className="flex group">
-      <div className={`tab p-med-90 ${isActive ? 'active' : ''} small`}>
+      <div className={`arrow-menu ${isActive ? 'active' : ''} small`}>
         <div className="flex space-x-8 items-center">
-          <span>{title}</span>
+          <span className="p-med-130 ">{title}</span>
           <MbIcon
             name={EIconName.ARROW_DROP_DOWN}
-            className="flex transform transition ease-in-out duration-300 group-hover:rotate-180 rotate-0"
+            className="flex transform transition ease-in-out duration-500 group-hover:rotate-180 rotate-0"
             size="16px"
-            color="blue-300"
-            darkColor="blue-100"
+            color="black dark:text-white group-hover:text-blue-300 dark:group-hover:text-blue-100 transition ease-in-out duration-500"
           />
         </div>
       </div>
