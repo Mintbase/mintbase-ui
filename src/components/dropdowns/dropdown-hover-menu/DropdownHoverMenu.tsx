428374424da3eac164c83c3d969538201e662ce4
Rui Santiago <ruisantiagomr@gmail.com>
2023-04-05 18:09:51 +0100

    :sparkles: Allow dropdownbutton prop in dropdownhovermenu component (#450)
    * :sparkles: Allow dropdownbutton prop in dropdownhovermenu component
    
    * update

--- src/components/dropdowns/dropdown-hover-menu/DropdownHoverMenu.tsx
+++ src/components/dropdowns/dropdown-hover-menu/DropdownHoverMenu.tsx
@@ -1,29 +1,63 @@
-import { MbArrowMenu } from '../../arrow-menu/ArrowMenu'
+import { useEffect, useRef } from 'react'
+import './dropdownHoverMenu.css'
 
 interface DropdownHoverMenuProps {
   className?: string
-  title: string
+  dropdownButton: JSX.Element
   children: JSX.Element
 }
 
 export const MbDropdownHoverMenu = (props: DropdownHoverMenuProps) => {
-  const { title, className, children } = props
+  const { className, children, dropdownButton } = props
+
+  const wrapperRef = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    if (wrapperRef && wrapperRef.current) {
+      const currentRef = wrapperRef.current
+      const bg = document.getElementById('dropdown-bg')
+
+      const showBg = () => {
+        bg?.classList.remove('hidden')
+        bg?.classList.add('block')
+      }
+
+      const hideBg = () => {
+        bg?.classList.remove('block')
+        bg?.classList.add('hidden')
+      }
+
+      currentRef.addEventListener('mouseenter', showBg)
+      currentRef.addEventListener('mouseleave', hideBg)
+      return () => {
+        currentRef.removeEventListener('mouseenter', showBg)
+        currentRef.removeEventListener('mouseleave', hideBg)
+      }
+    }
+  }, [wrapperRef])
 
   return (
-    <div id="dropdown-hover-menu" className="group h-auto w-min">
-      <MbArrowMenu title={title} />
+    <>
       <div
         id="dropdown-bg"
-        className="hidden group-hover:block fixed bg-black bg-opacity-30 overflow-y-auto h-screen right-0 left-0 z-30"
+        className="hidden fixed bg-black bg-opacity-30 overflow-y-auto h-screen right-0 left-0 z-30"
       ></div>
 
       <div
-        id="menu-items"
-        className={`invisible opacity-0 group-hover:opacity-100 group-hover:visible flex absolute flex-col
-          transition-all ease-in-out duration-500 bg-white dark:bg-gray-850 p-24 rounded-b z-40 ${className}`}
+        id="dropdown-hover-menu"
+        className="group h-auto w-min"
+        ref={wrapperRef}
       >
-        <div className="dark:text-white">{children}</div>
+        {dropdownButton}
+
+        <div
+          id="menu-items"
+          className={`invisible opacity-0 group-hover:opacity-100 group-hover:visible flex absolute flex-col
+          transition-all ease-in-out duration-500 bg-white dark:bg-gray-850 p-24 rounded-b z-40 ${className}`}
+        >
+          <div className="dark:text-white">{children}</div>
+        </div>
       </div>
-    </div>
+    </>
   )
 }
