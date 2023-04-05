a7faaf30c94dd728bb39bd8758ccaff0f3e9fb11
Rui Santiago <ruisantiagomr@gmail.com>
2023-04-05 17:15:49 +0100

    Fix hover bg (#449)
    * fix background hover
    
    * :lipstick: Add missing ID

--- src/components/dropdowns/dropdown-hover-menu/DropdownHoverMenu.tsx
+++ src/components/dropdowns/dropdown-hover-menu/DropdownHoverMenu.tsx
@@ -12,7 +12,11 @@ export const MbDropdownHoverMenu = (props: DropdownHoverMenuProps) => {
   return (
     <div id="dropdown-hover-menu" className="group h-auto w-min">
       <MbArrowMenu title={title} />
-      <div className="hidden group-hover:block fixed bg-black bg-opacity-30 overflow-y-auto h-screen right-0 left-0 z-30"></div>
+      <div
+        id="dropdown-bg"
+        className="hidden group-hover:block fixed bg-black bg-opacity-30 overflow-y-auto h-screen right-0 left-0 z-30"
+      ></div>
+
       <div
         id="menu-items"
         className={`invisible opacity-0 group-hover:opacity-100 group-hover:visible flex absolute flex-col
