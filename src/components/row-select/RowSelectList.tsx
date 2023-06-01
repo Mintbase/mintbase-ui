467f93adca77c2441c3cbc6b8dbbab504ddf72bb
Rui Santiago <ruisantiagomr@gmail.com>
2023-06-01 18:00:49 +0100

    :lipstick: Add background color to row select list component (#490)

--- src/components/row-select/RowSelectList.tsx
+++ src/components/row-select/RowSelectList.tsx
@@ -29,120 +29,122 @@ export const MbRowSelectList = ({
   }
 
   return (
-    <MbInputAccount
-      smallSubtitle=""
-      subtitle=""
-      inputs={
-        <MbTable>
-          <MbHead>
-            <tr>
-              <td>Account</td>
-            </tr>
-          </MbHead>
-          <MbBody>
-            {elements &&
-              elements.map((element) => {
-                const { id, content, isDisabled } = element
-                return (
-                  <tr
-                    key={id}
-                    className={`${
-                      isElementSelected(id)
-                        ? 'bg-blue-300-15 dark:bg-blue-100-15'
-                        : ''
-                    }`}
-                  >
-                    <td>
-                      <div className="flex items-center gap-12">
-                        {!isDisabled && (
-                          <MbCheckbox
-                            checked={isElementSelected(id)}
-                            onChange={(e) => {
-                              if (isElementSelected(id)) {
-                                setCheckedList(
-                                  checkedList.filter((elm) => elm !== id)
-                                )
-                                return
-                              }
-                              setCheckedList([...checkedList, id])
-                            }}
-                          />
-                        )}
+    <div className='bg-white dark:bg-gray-850 rounded'>
+      <MbInputAccount
+        smallSubtitle=""
+        subtitle=""
+        inputs={
+          <MbTable>
+            <MbHead>
+              <tr>
+                <td>Account</td>
+              </tr>
+            </MbHead>
+            <MbBody>
+              {elements &&
+                elements.map((element) => {
+                  const { id, content, isDisabled } = element
+                  return (
+                    <tr
+                      key={id}
+                      className={`${
+                        isElementSelected(id)
+                          ? 'bg-blue-300-15 dark:bg-blue-100-15'
+                          : ''
+                      }`}
+                    >
+                      <td>
+                        <div className="flex items-center gap-12">
+                          {!isDisabled && (
+                            <MbCheckbox
+                              checked={isElementSelected(id)}
+                              onChange={(e) => {
+                                if (isElementSelected(id)) {
+                                  setCheckedList(
+                                    checkedList.filter((elm) => elm !== id)
+                                  )
+                                  return
+                                }
+                                setCheckedList([...checkedList, id])
+                              }}
+                            />
+                          )}
 
-                        {content}
-                      </div>
-                    </td>
-                    {isDisabled ? (
-                      <></>
-                    ) : (
-                      checkedList.length < 1 && (
-                        <td>
-                          <div className="flex justify-end w-auto">
-                            <span
-                              className="cursor-pointer"
-                              onClick={() => deleteRow(id)}
-                            >
-                              <MbIcon
-                                name={EIconName.DELETE}
-                                size="24px"
-                                color="blue-300"
-                                darkColor="blue-100"
-                              />
-                            </span>
-                          </div>
-                        </td>
-                      )
-                    )}
-                  </tr>
-                )
-              })}
-          </MbBody>
-        </MbTable>
-      }
-      footer={
-        <div className="grid grid-cols-1 md:grid-cols-2 justify-between items-center w-full gap-24 px-24 pb-24">
-          <div className="grid grid-cols-2 gap-12 text-left">
-            <MbInfoCard
-              boxInfo={{
-                description: `${elements.length}`,
-                title: 'Accounts',
-              }}
-            />
-            {checkedList.length > 0 && (
+                          {content}
+                        </div>
+                      </td>
+                      {isDisabled ? (
+                        <></>
+                      ) : (
+                        checkedList.length < 1 && (
+                          <td>
+                            <div className="flex justify-end w-auto">
+                              <span
+                                className="cursor-pointer"
+                                onClick={() => deleteRow(id)}
+                              >
+                                <MbIcon
+                                  name={EIconName.DELETE}
+                                  size="24px"
+                                  color="blue-300"
+                                  darkColor="blue-100"
+                                />
+                              </span>
+                            </div>
+                          </td>
+                        )
+                      )}
+                    </tr>
+                  )
+                })}
+            </MbBody>
+          </MbTable>
+        }
+        footer={
+          <div className="grid grid-cols-1 md:grid-cols-2 justify-between items-center w-full gap-24 px-24 pb-24">
+            <div className="grid grid-cols-2 gap-12 text-left">
               <MbInfoCard
                 boxInfo={{
-                  description: `${checkedList.length}`,
-                  title: 'Selected Accounts',
+                  description: `${elements.length}`,
+                  title: 'Accounts',
                 }}
               />
-            )}
-          </div>
-          <div className="flex items-center justify-center md:justify-end gap-24">
-            {checkedList.length > 0 && (
-              <MbAction onClick={() => setCheckedList([])}>
-                <span>Clear Selection</span>
-              </MbAction>
-            )}
+              {checkedList.length > 0 && (
+                <MbInfoCard
+                  boxInfo={{
+                    description: `${checkedList.length}`,
+                    title: 'Selected Accounts',
+                  }}
+                />
+              )}
+            </div>
+            <div className="flex items-center justify-center md:justify-end gap-24">
+              {checkedList.length > 0 && (
+                <MbAction onClick={() => setCheckedList([])}>
+                  <span>Clear Selection</span>
+                </MbAction>
+              )}
 
-            {checkedList.length > 0 ? (
-              <MbButton
-                btnType={EType.PRIMARY}
-                state={EState.CAUTION}
-                label="Remove"
-                dropDownItems={dropdownItems}
-                onClick={() => removeMinters(checkedList)}
-              />
-            ) : (
-              <MbButton
-                btnType={EType.PRIMARY}
-                state={EState.ACTIVE}
-                label="Add Minters"
-                onClick={() => addMinters()}
-              />
-            )}
+              {checkedList.length > 0 ? (
+                <MbButton
+                  btnType={EType.PRIMARY}
+                  state={EState.CAUTION}
+                  label="Remove"
+                  dropDownItems={dropdownItems}
+                  onClick={() => removeMinters(checkedList)}
+                />
+              ) : (
+                <MbButton
+                  btnType={EType.PRIMARY}
+                  state={EState.ACTIVE}
+                  label="Add Minters"
+                  onClick={() => addMinters()}
+                />
+              )}
+            </div>
           </div>
-        </div>
-      }
-    />
+        }
+      />
+    </div>
   )
 }
--- src/stories/components/RowSelectList.stories.tsx
+++ src/stories/components/RowSelectList.stories.tsx
@@ -29,57 +29,7 @@ export const RowSelectList: ComponentStory<typeof MbRowSelectList> = () => (
         id: 'mintbase1.near',
         isDisabled: true,
       },
-      {
-        content: (
-          <div className="cursor-pointer flex items-center gap-12">
-            <div className="inline object-fit w-6 h-6 rounded-full overflow-hidden">
-              <img
-                className="h-full w-full object-cover"
-                src="https://i.imgur.com/gu26H6Z.png"
-              />
-            </div>
-            <MbText className="p-med-90 text-blue-300 dark:text-blue-100">
-              mintbase2.near
-            </MbText>
-          </div>
-        ),
-        id: 'mintbase2.near',
-        isDisabled: false,
-      },
-      {
-        content: (
-          <div className="cursor-pointer flex items-center gap-12">
-            <div className="inline object-fit w-6 h-6 rounded-full overflow-hidden">
-              <img
-                className="h-full w-full object-cover"
-                src="https://i.imgur.com/gu26H6Z.png"
-              />
-            </div>
-            <MbText className="p-med-90 text-blue-300 dark:text-blue-100">
-              mintbase3.near
-            </MbText>
-          </div>
-        ),
-        id: 'mintbase3.near',
-        isDisabled: false,
-      },
-      {
-        content: (
-          <div className="cursor-pointer flex items-center gap-12">
-            <div className="inline object-fit w-6 h-6 rounded-full overflow-hidden">
-              <img
-                className="h-full w-full object-cover"
-                src="https://i.imgur.com/gu26H6Z.png"
-              />
-            </div>
-            <MbText className="p-med-90 text-blue-300 dark:text-blue-100">
-              mintbase4.near
-            </MbText>
-          </div>
-        ),
-        id: 'mintbase4.near',
-        isDisabled: false,
-      },
+
     ]}
     deleteRow={() => null}
     addMinters={() => null}
